d28ddc7632098b81231db11b25a94df9566c219d
nhz2 <nhz2@cornell.edu>
2019-09-08 01:44:04 -0400

    Fixed time2datetime to handle leap seconds

--- MATLAB/utl/time2datetime.m
+++ MATLAB/utl/time2datetime.m
@@ -2,8 +2,7 @@ function out_datetime = time2datetime(time,init_GPS_week_number)
 %time2datetime converts time to a datetime with timezone 'UTCLeapSeconds'
 %   time(double): time since init_GPS_week_number in seconds
 %   init_GPS_week_number(int): initial GPS week number.
-nano_secs_gps= uint64(init_GPS_week_number)*7*24*3600*uint64(1E9)+uint64(time*1E9);
-out_datetime = datetime(nano_secs_gps,'ConvertFrom',...
-'epochtime','Epoch',datetime(1980,1,6,'TimeZone','UTCLeapSeconds'),'TicksPerSecond',1E9,'TimeZone','UTCLeapSeconds');
+pan_epoch=seconds(init_GPS_week_number*7*24*3600)+datetime(1980,1,6,'TimeZone','UTCLeapSeconds');
+out_datetime = seconds(time)+pan_epoch;
 end
 
