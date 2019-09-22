714630650d14ec7f6d50a9e57d3703519e1d2da2
nhz2 <nhz2@cornell.edu>
2019-09-22 15:45:50 -0400

    wip flight controller sim update

--- MATLAB/update_FC_state.m
+++ MATLAB/update_FC_state.m
@@ -1,4 +1,4 @@
-function [final_state,actuators] = update_FC_state(initial_state,const,sensors)
+function [final_state,actuators] = update_FC_state(initial_state,sensors)
 % Global variables treated as inputs:
 %  * const
 %  * sensors
@@ -10,15 +10,17 @@ function [final_state,actuators] = update_FC_state(initial_state,const,sensors)
 % attitude PD controller
 %   calculate commanded wheel torque
 %   update state with commanded wheel torque
-quat_cmd = utl_array2quaternion(initial_state.quat_cmd);
-quat_body_eci = utl_array2quaternion(initial_state.quat_body_eci);
+global const
+quat_cmd = [0;0;1;0;];%utl_array2quaternion(initial_state.quat_cmd);
+quat_body_eci = utl_array2quaternion(sensors.quat_body_eci);
 quat_error =  conj(quat_cmd) * quat_body_eci;
 if parts(quat_error) < 0
     quat_error = -quat_error;
 end
 
-[q1 q2 q3 q4] = utl_quaternion2array(quat_error);
-actuators.wheel_torque = [q1; q2; q3]*const.Kp + initial_state.angular_rate_body*const.Kd;
-
+q = utl_quaternion2array(quat_error);
+actuators.wheel_torque = q(1:3)*const.ATTITUDE_PD_KP + sensors.angular_rate_body*const.ATTITUDE_PD_KD;
+actuators.magrod_moment= [0;0;0;];
+final_state=initial_state;
 
 end
