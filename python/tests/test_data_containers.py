6c8cca7d50a692282345c6665b44aae7f8eac3d1
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2019-10-06 16:48:47 -0400

    Data container testing

--- python/tests/test_data_containers.py
+++ python/tests/test_data_containers.py
@@ -1,13 +1,14 @@
 from usb_console.data_containers import Datastore, Logger
 import unittest
-import os, json, tempfile
+import os, json, tempfile, time
 
 def test_datastore():
     tempdir = tempfile.mkdtemp() + "/datastore_test_dir"
     os.makedirs(tempdir, exist_ok=True)
     datastore = Datastore("test_device", tempdir)
     datastore.start()
-    datastore.put({"field_name": "test_field", "val" : "val", "time" : "2"})
+    datastore.put({"field": "test_field", "val" : "val", "time" : "2"})
+    time.sleep(1.5) # Allow for some time for the data to be processed by the queue processor
     datastore.stop()
 
     with open(tempdir + "/test_device-telemetry.txt","r") as datafile:
@@ -16,8 +17,8 @@ def test_datastore():
         assert len(data['test_field']) == 1
 
         datapoint = data['test_field'][0]
-        assert datapoint['time'] == "2"
-        assert datapoint['val'] == "val"
+        assert datapoint[0] == "2"
+        assert datapoint[1] == "val"
 
 def test_logger():
     tempdir = tempfile.mkdtemp() + "/logger_test_dir"
@@ -25,6 +26,7 @@ def test_logger():
     logger = Logger("test_device", tempdir)
     logger.start()
     logger.put("Hello world")
+    time.sleep(1.5) # Allow for some time for the data to be processed by the queue processor
     logger.stop()
 
     with open(tempdir + "/test_device-log.txt", "r") as logfile:
