73cb71d85e1e00e61cdeebd17f26b82926b39379
nhz2 <nhz2@cornell.edu>
2019-10-05 20:58:00 -0400

    removed unused function

--- MATLAB/actuator_state_update.m
+++
@@ -1,7 +0,0 @@
-function [final_actuator_state] = actuator_state_update(initial_actuator_state,commanded_actuators,state,delta_time)
-%UNTITLED2 Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
-
