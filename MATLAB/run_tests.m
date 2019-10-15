ad78358e2318e372c4439ba84a51030a4116d053
nhz2 <nhz2@cornell.edu>
2019-10-15 17:18:11 -0400

    added tests for sensors_update.m and dynamics_update.m

--- MATLAB/run_tests.m
+++ MATLAB/run_tests.m
@@ -3,5 +3,7 @@ runtests('test_env_sun_vector');
 runtests('test_env_earth_attitude');
 runtests('test_initialize_main_state');
 runtests('test_main_state_update');
+runtests('test_dynamics_update');
+runtests('test_sensors_update');
 runtests('test_sensor_reading');
 runtests('test_actuator_command');
