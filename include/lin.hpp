20073300bb658ea9d3d4f90fafa7d95d9b77e6dc
kkrol27 <kyle.krol.27@gmail.com>
2019-10-27 15:22:01 -0700

    Update to track off lin/master and add lin.hpp

---
+++ include/lin.hpp
@@ -0,0 +1,21 @@
+//
+// include/gnc_utilities.hpp
+// PSim
+//
+// Contributors:
+//   Kyle Krol  kpk63@cornell.edu
+//
+// Pathfinder for Autonomous Navigation
+// Space Systems Design Studio
+// Cornell Univeristy
+//
+
+#ifndef PAN_PSIM_INCLUDE_LIN_HPP_
+#define PAN_PSIM_INCLUDE_LIN_HPP_
+
+#include "../lin/include/core/core.hpp"
+
+#include "../lin/include/algorithms/backward_sub.hpp"
+#include "../lin/include/algorithms/qr.hpp"
+
+#endif
