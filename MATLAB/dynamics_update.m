cbdcc99272392ef10e65e519237902ea794863b4
srutiv <srutiv98@gmail.com>
2019-10-31 13:46:29 -0400

    Update dynamics_update.m

--- MATLAB/dynamics_update.m
+++ MATLAB/dynamics_update.m
@@ -98,7 +98,7 @@ delta_time= double(const.dt)*1E-9;
         g_eci=rotateframe(quat_eci_ecef,g_ecef')';
         %TODO add thruster firings forces and torques
         %TODO add drag and solar pressure forces
-        [F_envdrag,rho,Cd,A,H]  = env_atmospheric_drag(t,y(1:3),r_ecef,y(4:6)); %drag force; input mission time in sec, position ECI m, and velocity ECI m/s
+        [F_envdrag, A]  = env_atmospheric_drag(y(1:3),pos_ecef,y(4:6)); %drag force; input mission time in sec, position ECI m, and velocity ECI m/s
         dydt(4:6)= g_eci + F_envdrag/(const.MASS+fuel_mass(t));
         quat_rate=quaternion(0,y(11),y(12),y(13));
         dydt(7:10)= utl_quaternion2array(0.5*quat_body_eci*quat_rate);
