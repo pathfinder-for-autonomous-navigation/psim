e1149a538c3579059fcfc8717eaba074b13d6fd6
kkrol27 <kyle.krol.27@gmail.com>
2019-11-08 18:34:16 -0800

    Initial attitude estimation implementation

---
+++ test/attitude_estimation_test/test.cpp
@@ -0,0 +1,31 @@
+//
+// test/attitude_estimation_test/test.cpp
+// PSim
+//
+// Contributors:
+//   Kyle Krol  kpk63@cornell.edu
+//
+// Pathfinder for Autonomous Navigation
+// Space Systems Design Studio
+// Cornell Univeristy
+//
+
+#include "../test.hpp"
+
+#include <gnc_attitude_estimation.hpp>
+
+#include <cmath>
+
+void test_nan_intputs() {
+  gnc::AttitudeEstimatorState state;
+  gnc::AttitudeEstimatorData data;
+  gnc::AttitudeEstimate estimate;
+  gnc::estimate_attitude(state, data, estimate);
+  // Assert all NaN outputs
+  TEST_ASSERT_TRUE(std::isnan(estimate.q_body_eci(0)));
+  TEST_ASSERT_TRUE(std::isnan(estimate.w_body(0)));
+}
+
+void test() {
+  RUN_TEST(test_nan_intputs);
+}
