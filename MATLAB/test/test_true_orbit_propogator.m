90cddc6d14ff1920ecb6f6838ddd6c3b0a4e5d9b
srutiv <srutiv98@gmail.com>
2019-11-13 05:55:04 -0500

    comparison with GRACE data

--- MATLAB/test/test_true_orbit_propogator.m
+++ MATLAB/test/test_true_orbit_propogator.m
@@ -1,80 +1,203 @@
 config();
+tic
+%load GRACE data; in m, m/s, ECEF
+data = csvread('graceClean.csv',1,0);
+%num_states = length(data);
+num_states = 3;
+global const
 
-%[t_array, states, orb_elemsf] = true_orbit_propagator([(500E3+6.37814E06) (500E3+6.37814E06) (500E3+6.37814E06)], [5 6 7], 0, 1.314E7);
-[t_array, states, orb_elemsf] = true_orbit_propagator(1E3.*[2067829.846415895 776905.9724453629 0.0009578283838282736], [0.0006236271904723294 0.0006257082914522712 941.0211143841228], 0, 1.314E7);
+%convert to from ECEF --> ECI
+for i = 1:num_states
+   %convert time to Julian Date Vector
+   datetime = utl_time2datetimeUTC(data(i,1),0); %second input should be init_GPS_week_number(int): initial GPS week number.  
+   jd = juliandate(datetime); %default timezone is UTC
+   
+   [r_ECI, v_ECI] = ECEFtoECI(jd,data(i,2:4)',data(i,8:10)');
+   data(i,2:4) = r_ECI; %convert position in ECEF to ECI
+   data(i,8:10) = v_ECI; %convert velocity in ECEF to ECI
+end
 
-%PLOTS
-figure(1);
+fprintf('converted GRACE data from ECEF --> ECI \n')
 
-subplot(5,2,1)
-plot(t_array,orb_elemsf(:,1),'LineWidth',1); 
-axis equal; shg                     
-ylabel('a km'); 
-xlabel('time (s)'); 
-title('a semi major axis km');
+%getting orbital elements for GRACE
+orb_elemsg = zeros(num_states,10); 
+t_array = linspace(data(1,1),data(1,1)+num_states,1);
+for count = 1:num_states
+    rf  = data(count,2:4)'; %new position 
+    vf = data(count,8:10)'; %new velocity 
+    %quatf = states(count,7:10); %new quaternions
 
-subplot(5,2,2)
-plot(t_array,orb_elemsf(:,2),'LineWidth',1); 
-axis equal; shg                     
-ylabel('eMag'); 
-xlabel('time (s)');
-title('eccentricity');
-
-subplot(5,2,3)
-plot(t_array,orb_elemsf(:,3),'LineWidth',1); 
-axis equal; shg                     
-ylabel('i radians'); 
-xlabel('time (s)');
-title('i inclination radians');
-
-subplot(5,2,4)
-plot(t_array,orb_elemsf(:,4),'LineWidth',1); 
-axis equal; shg                     
-ylabel('O radians'); 
-xlabel('time (s)');
-title('O RAAN radians'); 
-
-subplot(5,2,5)
-plot(t_array,orb_elemsf(:,5),'LineWidth',1); 
-axis equal; shg                     
-ylabel('o radians'); 
-xlabel('time (s)');
-title('o argument of perigee radians');
-
-subplot(5,2,6)
-plot(t_array,orb_elemsf(:,6),'LineWidth',1); 
-axis equal; shg                     
-ylabel('M radians'); 
-xlabel('time (s)');
-title('M mean anomaly radians');
-
-subplot(5,2,7)
-plot(t_array,orb_elemsf(:,7),'LineWidth',1); 
-axis equal; shg                     
-ylabel('trueLon radians'); 
-xlabel('time (s)');
-title('trueLon true longitude radians'); 
-
-subplot(5,2,8)
-plot(t_array,orb_elemsf(:,8),'LineWidth',1); 
-axis equal; shg                     
-ylabel('argLat radians'); 
-xlabel('time (s)');
-title('argLat radians');
-
-subplot(5,2,9)
-plot(t_array,orb_elemsf(:,9),'LineWidth',1); 
-axis equal; shg                     
-ylabel('lonPer radians'); 
-xlabel('time (s)');
-title('lonPer radians'); 
-
-subplot(5,2,10)
-plot(t_array,orb_elemsf(:,10),'LineWidth',1); 
-axis equal; shg                     
-ylabel('p km'); 
-xlabel('time (s)');
-title('p semilatus rectum km');
+    %calculate orbital elements
+    [a, eMag, i, O, o, nu, truLon, argLat, lonPer, p] = utl_rv2orb(rf, vf, const.mu);
+    orb_elemsg(count,1) = a*1E3; orb_elemsg(count,2) = eMag; orb_elemsg(count,3) = i;
+    orb_elemsg(count,4) = O; orb_elemsg(count,5) = o; orb_elemsg(count,6) = nu;
+    orb_elemsg(count,7) = truLon; orb_elemsg(count,8) = argLat; orb_elemsg(count,9) = lonPer;
+    orb_elemsg(count,10) = p*1E3;
+end
 
 
+% run true orbit propogator with GRACE initial data
+% true_orbit_propagator(initial pos, initial vel, start_time in sec, duration in sec);
+[t_array, states, orb_elemsf] = true_orbit_propagator(data(1,2:4), data(1,8:10), data(1,1), length(data));
+%[t_array, states, orb_elemsf] = true_orbit_propagator(data(1,1:3), data(1,7:9), 0, 1.314E7);
 
+%%%%compare true orbital states with GRACE states
+err = zeros(num_states,6);
+
+for i = 1:num_states
+    x_grace = data(i,2);
+    y_grace = data(i,3);
+    z_grace = data(i,4);
+    xdot_grace = data(i,8);
+    ydot_grace = data(i,9);
+    zdot_grace = data(i,10);
+    
+    x = states(i,1);
+    y = states(i,2);
+    z = states(i,3);
+    xdot = states(i,4);
+    ydot = states(i,5);
+    zdot = states(i,6);
+    
+    err(i,1) = abs(x_grace-x)/x_grace;
+    err(i,2) = abs(y_grace-y)/y_grace;
+    err(i,3) = abs(z_grace-z)/z_grace;
+    err(i,4) = abs(xdot_grace-xdot)/xdot_grace;
+    err(i,5) = abs(ydot_grace-ydot)/ydot_grace;
+    err(i,6) = abs(zdot_grace-zdot)/zdot_grace;
+end
+
+avg_xerr = mean(err(:,1));
+avg_yerr = mean(err(:,2));
+avg_zerr = mean(err(:,3));
+avg_xdoterr = mean(err(:,4));
+avg_ydoterr = mean(err(:,5));
+avg_zdoterr = mean(err(:,6));
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%PLOTS for true orbit propogator
+% figure(1)
+% 
+% subplot(5,2,1)
+% loglog(t_array,orb_elemsf(:,1),'LineWidth',1);                     
+% ylabel('a'); 
+% xlabel('time (s)'); 
+% title('a semi major axis');
+% 
+% subplot(5,2,2)
+% loglog(t_array,orb_elemsf(:,2),'LineWidth',1);                    
+% ylabel('eMag'); 
+% xlabel('time (s)');
+% title('eccentricity');
+% 
+% subplot(5,2,3)
+% loglog(t_array,orb_elemsf(:,3),'LineWidth',1);                   
+% ylabel('i radians'); 
+% xlabel('time (s)');
+% title('i inclination radians');
+% 
+% subplot(5,2,4)
+% loglog(t_array,orb_elemsf(:,4),'LineWidth',1);                     
+% ylabel('O radians'); 
+% xlabel('time (s)');
+% title('O RAAN radians'); 
+% 
+% subplot(5,2,5)
+% loglog(t_array,orb_elemsf(:,5),'LineWidth',1);                     
+% ylabel('o radians'); 
+% xlabel('time (s)');
+% title('o argument of perigee radians');
+% 
+% subplot(5,2,6)
+% loglog(t_array,orb_elemsf(:,6),'LineWidth',1);                     
+% ylabel('M radians'); 
+% xlabel('time (s)');
+% title('M mean anomaly radians');
+% 
+% subplot(5,2,7)
+% loglog(t_array,orb_elemsf(:,7),'LineWidth',1);                   
+% ylabel('trueLon radians'); 
+% xlabel('time (s)');
+% title('trueLon true longitude radians'); 
+% 
+% subplot(5,2,8)
+% loglog(t_array,orb_elemsf(:,8),'LineWidth',1);                
+% ylabel('argLat radians'); 
+% xlabel('time (s)');
+% title('argLat radians');
+% 
+% subplot(5,2,9)
+% loglog(t_array,orb_elemsf(:,9),'LineWidth',1);                      
+% ylabel('lonPer radians'); 
+% xlabel('time (s)');
+% title('lonPer radians'); 
+% 
+% subplot(5,2,10)
+% loglog(t_array,orb_elemsf(:,10),'LineWidth',1);                     
+% ylabel('p'); 
+% xlabel('time (s)');
+% title('p semilatus rectum');
+
+% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%PLOTS for GRACE
+% figure(2);
+% 
+% subplot(5,2,1)
+% loglog(t_array,orb_elemsg(:,1),'LineWidth',1);                     
+% ylabel('a'); 
+% xlabel('time (s)'); 
+% title('a semi major axis');
+% 
+% subplot(5,2,2)
+% loglog(t_array,orb_elemsg(:,2),'LineWidth',1);                     
+% ylabel('eMag'); 
+% xlabel('time (s)');
+% title('eccentricity');
+% 
+% subplot(5,2,3)
+% loglog(t_array,orb_elemsg(:,3),'LineWidth',1);                      
+% ylabel('i radians'); 
+% xlabel('time (s)');
+% title('i inclination radians');
+% 
+% subplot(5,2,4)
+% loglog(t_array,orb_elemsg(:,4),'LineWidth',1);                    
+% ylabel('O radians'); 
+% xlabel('time (s)');
+% title('O RAAN radians'); 
+% 
+% subplot(5,2,5)
+% loglog(t_array,orb_elemsg(:,5),'LineWidth',1);                   
+% ylabel('o radians'); 
+% xlabel('time (s)');
+% title('o argument of perigee radians');
+% 
+% subplot(5,2,6)
+% loglog(t_array,orb_elemsg(:,6),'LineWidth',1);                    
+% ylabel('M radians'); 
+% xlabel('time (s)');
+% title('M mean anomaly radians');
+% 
+% subplot(5,2,7)
+% loglog(t_array,orb_elemsg(:,7),'LineWidth',1);                     
+% ylabel('trueLon radians'); 
+% xlabel('time (s)');
+% title('trueLon true longitude radians'); 
+% 
+% subplot(5,2,8)
+% loglog(t_array,orb_elemsg(:,8),'LineWidth',1);                   
+% ylabel('argLat radians'); 
+% xlabel('time (s)');
+% title('argLat radians');
+% 
+% subplot(5,2,9)
+% loglog(t_array,orb_elemsg(:,9),'LineWidth',1);                      
+% ylabel('lonPer radians'); 
+% xlabel('time (s)');
+% title('lonPer radians'); 
+% 
+% subplot(5,2,10)
+% loglog(t_array,orb_elemsg(:,10),'LineWidth',1);                     
+% ylabel('p'); 
+% xlabel('time (s)');
+% title('p semilatus rectum');
+%  
+toc
--- MATLAB/true_orbit_propagator.m
+++ MATLAB/true_orbit_propagator.m
@@ -1,78 +1,100 @@
-function [t_array, states, orb_elemsf] = true_orbit_propagator(r,v,current_time,delta_time)
-    %INPUT r, v in ECI coordinates
-    %OUTPUTS r, v in ECI coordinates at time = current_time + delta_time
+function [t_array, states, orb_elemsf] = true_orbit_propagator(r,v,start_time,duration)
+    %INPUT r, v in ECI coordinates in m and m/s
+    %OUTPUTS r, v in ECI coordinates in m and m/s at time = current_time + delta_time
 
     global const
     
-    %intialize for initial gravitational acceleration
-    %[quat_ecef_eci_initial,~]=env_earth_attitude(current_time);
-    %[quat_ecef_eci_final,~]=env_earth_attitude(current_time+delta_time);
-    %quat_ecef_eci_initial= utl_array2quaternion(quat_ecef_eci_initial);
-    %quat_ecef_eci_final= utl_array2quaternion(quat_ecef_eci_final);
 
     %package initial state to call ODE45
     state0 = zeros([6,1]);
     state0(1:3)= r;
     state0(4:6)= v;
-
+    
+%     %intialize for initial gravitational acceleration
+%     [quat_ecef_eci_initial,~]=env_earth_attitude(current_time);
+%     [quat_ecef_eci_final,~]=env_earth_attitude(current_time+delta_time);
+%     quat_ecef_eci_initial= utl_array2quaternion(quat_ecef_eci_initial);
+%     quat_ecef_eci_final= utl_array2quaternion(quat_ecef_eci_final);
+%     state0(7:10) = quat_body_eci
+    
+    
     %create tarray
-    t_array= linspace(current_time, (current_time + delta_time), 10000);
-
-    [t_array states] = ode45(@state_dot, t_array, state0);
+    opts = odeset('RelTol', 1E-12, 'AbsTol', 1E-3);
+    [t_array, states] = ode113(@state_dot, [start_time, (start_time + duration)], state0, opts);
     %statef = utl_ode2(@state_dot,[current_time,(current_time + delta_time)],state0);
     
     orb_elemsf = zeros(length(t_array)); 
     for count = 1:length(t_array)
         rf  = states(count,1:3)'; %new position 
         vf = states(count,4:6)'; %new velocity 
+        %quatf = states(count,7:10); %new quaternions
         
         %calculate orbital elements
         [a, eMag, i, O, o, nu, truLon, argLat, lonPer, p] = utl_rv2orb(rf, vf, const.mu);
-        orb_elemsf(count,1) = a; orb_elemsf(count,2) = eMag; orb_elemsf(count,3) = i;
+        orb_elemsf(count,1) = a*1E3; orb_elemsf(count,2) = eMag; orb_elemsf(count,3) = i;
         orb_elemsf(count,4) = O; orb_elemsf(count,5) = o; orb_elemsf(count,6) = nu;
         orb_elemsf(count,7) = truLon; orb_elemsf(count,8) = argLat; orb_elemsf(count,9) = lonPer;
-        orb_elemsf(count,10) = p;
+        orb_elemsf(count,10) = p*1E3;
     end
 end
 
-function statef = state_dot(t_array, state0)
+function statef = state_dot(t, state0)
         %y = [x; y; z; xdot; ydot; zdot]
         
         global const
         
         % unpack
         r = state0(1:3);  v = state0(4:6);
-        
-%         quat_ecef_eci= earth_first_order_rotation(t_array, current_time, delta_time);
-%         quat_eci_ecef= conj(quat_ecef_eci);
-%         pos_eci= v;
-%         pos_ecef=rotateframe(quat_ecef_eci,pos_eci')';
-%         [g_ecef,~,~]= env_gravity(current_time,pos_ecef);
-%         g_eci=rotateframe(quat_eci_ecef,g_ecef')';
     
         %calculate drag force
-        [F_envdrag, A] = env_atmospheric_drag(t_array, r,v); 
+        [F_envdrag, A] = env_atmospheric_drag(t, r,v); 
 
         %calculate solar pressure acceleration
-        acc_solrad = solradpressure(r,const.rp_earth,A,const.MASS); %returns acceleration in ECI
+        acc_solrad = env_solradpressure(r,const.rp_earth,A,const.MASS); %returns acceleration in ECI
+        
+        %use perihelion date instead of t_array?
+        datetime = utl_time2datetimeUTC(t,0); %second input should be init_GPS_week_number(int): initial GPS week number.  
+        
+        [rp_earth_moon,~] = planetEphemeris(juliandate(datetime),'Moon','Earth');
+        rp_earth_moon = 1E3*rp_earth_moon'; %positional vector from Moon to Earth; used for 3rd body perturb calcs
+        [rp_earth,~] = planetEphemeris(juliandate(datetime),'Sun','Earth');
+        rp_earth = 1E3*rp_earth; %positional vector from Sun to Earth; used for 3rd body perturb and solar radiation pressure calcs
+    
 
         %secular perturbations from Moon (third body in circular orbit)
-        acc_tbmoon = -const.mu_moon*(((r-const.rp_earth_moon)/norm(r-const.rp_earth_moon)^3) - (const.rp_earth_moon/norm(const.rp_earth_moon)^3));
+        acc_tbmoon = -const.mu_moon*(((r-rp_earth_moon)/norm(r-rp_earth_moon)^3) - (rp_earth_moon/norm(rp_earth_moon)^3));
         %returns acceleration in ECI
 
         %secular perturbations from Sun (third body in circular orbit)
-        acc_tbsun = -const.mu_sun*(((r-const.rp_earth')/norm(r-const.rp_earth')^3) - (const.rp_earth'/norm(const.rp_earth')^3));
+        acc_tbsun = -const.mu_sun*(((r-rp_earth')/norm(r-rp_earth')^3) - (rp_earth'/norm(rp_earth')^3));
         %returns acceleration in ECI
 
         % perturbations due to J-coefficients
-        acc_Js = gravitysphericalharmonic(r', 'EGM2008',10);
         %returns acceleration in ECI
+        acc_Js = gravitysphericalharmonic(r', 'EGM2008',10);
+        
+%         % gravitational force in ECI
+%         quat_ecef_eci= earth_first_order_rotation(t_array);
+%         quat_eci_ecef= conj(quat_ecef_eci);
+%         quat_body_eci=utl_array2quaternion(state0(7:10));
+%         quat_eci_body= conj(quat_body_eci);
+%         quat_body_ecef=quat_eci_ecef*quat_body_eci;
+%         pos_eci=state0(1:3);
+%         pos_ecef=rotateframe(quat_ecef_eci,pos_eci')';
+%         [g_ecef,~,G_ecef]= env_gravity(t_array,pos_ecef);
+%         g_eci=rotateframe(quat_eci_ecef,g_ecef')';
 
         statef = zeros([6,1]);
         statef(1:3)=state0(4:6);
         
+        
+        %acceration in m/s^2, ECI coords
         statef(4:6) = F_envdrag./(const.MASS) + acc_solrad + acc_tbmoon + acc_tbsun + acc_Js;
         %statef(4:6) = F_envdrag./(const.MASS) + acc_solrad + acc_tbmoon + acc_tbsun;
+        
+        %statef(7:10) = 
+        
+        
 end
 
 function quat_ecef_eci= earth_first_order_rotation(t, current_time, delta_time)
