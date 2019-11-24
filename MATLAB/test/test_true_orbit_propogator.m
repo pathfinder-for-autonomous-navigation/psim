54fe126bc61761994c2b6991e3b1fa655bf94c70
srutiv <srutiv98@gmail.com>
2019-11-24 14:57:17 -0500

    ticker

--- MATLAB/test/test_true_orbit_propogator.m
+++ MATLAB/test/test_true_orbit_propogator.m
@@ -4,12 +4,12 @@ config();
 data = csvread('graceClean.csv',1,0);
 
 %duration of the mission you want to simulate
-num_states = 5400; %1.5 hours i.e approximately 1 orbit %length(data) for 1 day
+num_states = 1.577E7; %1.5 hours i.e approximately 1 orbit %length(data) for 1 day
 
 global const
 
 %convert to from ECEF --> ECI
-for i = 1:num_states
+for i = 1:length(data)
    %GRACE time is gps_time in continuous seconds past 01-Jan-2000 11:59:47 UTC
    
    %convert GRACE time to PAN time
@@ -25,9 +25,9 @@ end
 fprintf('converted GRACE data from ECEF --> ECI \n')
 
 %getting orbital elements for GRACE
-orb_elemsg = zeros(num_states,10); 
-t_array = linspace(data(1,1),data(1,1)+num_states,1);
-for count = 1:num_states
+orb_elemsg = zeros(length(data),10); 
+t_array = linspace(data(1,1),data(1,1)+length(data),1);
+for count = 1:length(data)
     rf  = data(count,2:4)'; %new position 
     vf = data(count,8:10)'; %new velocity 
     %quatf = states(count,7:10); %new quaternions
@@ -40,6 +40,7 @@ for count = 1:num_states
     orb_elemsg(count,10) = p*1E3;
 end
 
+fprintf('got GRACE orbital elements \n')
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %test cases: [drag solrad 3rdbodymoon 3rdbodysun numberofJ's]
@@ -49,60 +50,60 @@ end
 
 
 %simplest model, J2 only
-tic
-perturbs.drag = 0;
-perturbs.solrad = 0;
-perturbs.bodmoon = 0;
-perturbs.bodsun = 0;
-perturbs.numJs = 2;
-
-[t_array1, states1, orb_elemsf1] = true_orbit_propagator(data(1,2:4), data(1,8:10), data(1,1), num_states, perturbs);
-err1 = get_error(data, states1);
-saveas(gcf,'test1.png')
-time1 = toc;
-
-%J4
-tic
-perturbs.drag = 0;
-perturbs.solrad = 0;
-perturbs.bodmoon = 0;
-perturbs.bodsun = 0;
-perturbs.numJs = 4;
-
-[t_array2, states2, orb_elemsf2] = true_orbit_propagator(data(1,2:4), data(1,8:10), data(1,1), num_states, perturbs);
-err2 = get_error(data, states2);
-saveas(gcf,'test2.png')
-time2 = toc;
- 
-%J6
-tic
-perturbs.drag = 0;
-perturbs.solrad = 0;
-perturbs.bodmoon = 0;
-perturbs.bodsun = 0;
-perturbs.numJs = 6;
-
-[t_array3, states3, orb_elemsf3] = true_orbit_propagator(data(1,2:4), data(1,8:10), data(1,1), num_states, perturbs);
-err3 = get_error(data, states3);
-saveas(gcf,'test3.png')
-time3 = toc;
-
-%J8
-tic
-perturbs.drag = 0;
-perturbs.solrad = 0;
-perturbs.bodmoon = 0;
-perturbs.bodsun = 0;
-perturbs.numJs = 8;
-
-[t_array4, states4, orb_elemsf4] = true_orbit_propagator(data(1,2:4), data(1,8:10), data(1,1), num_states, perturbs);
-err4 = get_error(data, states4);
-saveas(gcf,'test4.png')
-time4 = toc;
-
+% tic
+% perturbs.drag = 0;
+% perturbs.solrad = 0;
+% perturbs.bodmoon = 0;
+% perturbs.bodsun = 0;
+% perturbs.numJs = 2;
+% 
+% [t_array1, states1, orb_elemsf1] = true_orbit_propagator(data(1,2:4), data(1,8:10), data(1,1), num_states, perturbs);
+% err1 = get_error(data, states1);
+% saveas(gcf,'test1.png')
+% time1 = toc;
+% 
+% %J4
+% tic
+% perturbs.drag = 0;
+% perturbs.solrad = 0;
+% perturbs.bodmoon = 0;
+% perturbs.bodsun = 0;
+% perturbs.numJs = 4;
+% 
+% [t_array2, states2, orb_elemsf2] = true_orbit_propagator(data(1,2:4), data(1,8:10), data(1,1), num_states, perturbs);
+% err2 = get_error(data, states2);
+% saveas(gcf,'test2.png')
+% time2 = toc;
+%  
+% %J6
+% tic
+% perturbs.drag = 0;
+% perturbs.solrad = 0;
+% perturbs.bodmoon = 0;
+% perturbs.bodsun = 0;
+% perturbs.numJs = 6;
+% 
+% [t_array3, states3, orb_elemsf3] = true_orbit_propagator(data(1,2:4), data(1,8:10), data(1,1), num_states, perturbs);
+% err3 = get_error(data, states3);
+% saveas(gcf,'test3.png')
+% time3 = toc;
+% 
+% %J8
+% tic
+% perturbs.drag = 0;
+% perturbs.solrad = 0;
+% perturbs.bodmoon = 0;
+% perturbs.bodsun = 0;
+% perturbs.numJs = 8;
+% 
+% [t_array4, states4, orb_elemsf4] = true_orbit_propagator(data(1,2:4), data(1,8:10), data(1,1), num_states, perturbs);
+% err4 = get_error(data, states4);
+% saveas(gcf,'test4.png')
+% time4 = toc;
+% 
 %J10
 tic
-perturbs.drag = 0;
+perturbs.drag = 1;
 perturbs.solrad = 0;
 perturbs.bodmoon = 0;
 perturbs.bodsun = 0;
@@ -112,72 +113,76 @@ perturbs.numJs = 10;
 err5 = get_error(data, states5);
 saveas(gcf,'test5.png')
 time5 = toc;
+% 
+% %test drag
+% tic
+% perturbs.drag = 1;
+% perturbs.solrad = 0;
+% perturbs.bodmoon = 0;
+% perturbs.bodsun = 0;
+% perturbs.numJs = 10;
+% 
+% [t_array6, states6, orb_elemsf6] = true_orbit_propagator(data(1,2:4), data(1,8:10), data(1,1), num_states, perturbs);
+% err6 = get_error(data, states6);
+% saveas(gcf,'test6.png')
+% time6 = toc;
+% 
+% 
+% %test solrad
+% tic
+% perturbs.drag = 0;
+% perturbs.solrad = 1;
+% perturbs.bodmoon = 0;
+% perturbs.bodsun = 0;
+% perturbs.numJs = 10;
+% 
+% [t_array7, states7, orb_elemsf7] = true_orbit_propagator(data(1,2:4), data(1,8:10), data(1,1), num_states, perturbs);
+% err7 = get_error(data, states7);
+% saveas(gcf,'test7.png')
+% time7 = toc;
+% 
+% %test 3rd body moon
+% tic
+% perturbs.drag = 0;
+% perturbs.solrad = 0;
+% perturbs.bodmoon = 1;
+% perturbs.bodsun = 0;
+% perturbs.numJs = 10;
+% 
+% [t_array8, states8, orb_elemsf8] = true_orbit_propagator(data(1,2:4), data(1,8:10), data(1,1), num_states, perturbs);
+% err8 = get_error(data, states8);
+% saveas(gcf,'test8.png')
+% time8 = toc;
+% 
+% %test 3rd body sun
+% tic
+% perturbs.drag = 0;
+% perturbs.solrad = 0;
+% perturbs.bodmoon = 0;
+% perturbs.bodsun = 1;
+% perturbs.numJs = 10;
+% 
+% [t_array9, states9, orb_elemsf9] = true_orbit_propagator(data(1,2:4), data(1,8:10), data(1,1), num_states, perturbs);
+% err9 = get_error(data, states9);
+% saveas(gcf,'test9.png')
+% time9 = toc;
+% 
+% %high fidelity
+% tic
+% perturbs.drag = 1;
+% perturbs.solrad = 1;
+% perturbs.bodmoon = 1;
+% perturbs.bodsun = 1;
+% perturbs.numJs = 10;
+% 
+% [t_array10, states10, orb_elemsf10] = true_orbit_propagator(data(1,2:4), data(1,8:10), data(1,1), num_states, perturbs);
+% err10 = get_error(data, states10);
+% saveas(gcf,'test10.png')
+% time10 = toc;
 
-%test drag
-tic
-perturbs.drag = 1;
-perturbs.solrad = 0;
-perturbs.bodmoon = 0;
-perturbs.bodsun = 0;
-perturbs.numJs = 10;
-
-[t_array6, states6, orb_elemsf6] = true_orbit_propagator(data(1,2:4), data(1,8:10), data(1,1), num_states, perturbs);
-err6 = get_error(data, states6);
-saveas(gcf,'test6.png')
-time6 = toc;
-
-
-%test solrad
-tic
-perturbs.drag = 0;
-perturbs.solrad = 1;
-perturbs.bodmoon = 0;
-perturbs.bodsun = 0;
-perturbs.numJs = 10;
-
-[t_array7, states7, orb_elemsf7] = true_orbit_propagator(data(1,2:4), data(1,8:10), data(1,1), num_states, perturbs);
-err7 = get_error(data, states7);
-saveas(gcf,'test7.png')
-time7 = toc;
-
-%test 3rd body moon
-tic
-perturbs.drag = 0;
-perturbs.solrad = 0;
-perturbs.bodmoon = 1;
-perturbs.bodsun = 0;
-perturbs.numJs = 10;
-
-[t_array8, states8, orb_elemsf8] = true_orbit_propagator(data(1,2:4), data(1,8:10), data(1,1), num_states, perturbs);
-err8 = get_error(data, states8);
-saveas(gcf,'test8.png')
-time8 = toc;
-
-%test 3rd body sun
-tic
-perturbs.drag = 0;
-perturbs.solrad = 0;
-perturbs.bodmoon = 0;
-perturbs.bodsun = 1;
-perturbs.numJs = 10;
-
-[t_array9, states9, orb_elemsf9] = true_orbit_propagator(data(1,2:4), data(1,8:10), data(1,1), num_states, perturbs);
-err9 = get_error(data, states9);
-saveas(gcf,'test9.png')
-time9 = toc;
-
-%high fidelity
-tic
-perturbs.drag = 1;
-perturbs.solrad = 1;
-perturbs.bodmoon = 1;
-perturbs.bodsun = 1;
-perturbs.numJs = 10;
-
-[t_array10, states10, orb_elemsf10] = true_orbit_propagator(data(1,2:4), data(1,8:10), data(1,1), num_states, perturbs);
-err10 = get_error(data, states10);
-saveas(gcf,'test10.png')
-time10 = toc;
+% getPlots(t_array,orb_elemsg) 
+% getPlots(t_array,orb_elemsf1) 
+% getPlots(t_array,orb_elemsf10) 
 
 
 %%%%compare true orbital states with GRACE states
