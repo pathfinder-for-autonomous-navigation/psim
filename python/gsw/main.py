097807507374cfc3d7879cddced07b424abeef60
Fatima Yousuf <fatimayousuf@dhcp-clara-3941.redrover.cornell.edu>
2019-11-23 23:54:52 -0500

    .

--- python/gsw/main.py
+++ python/gsw/main.py
@@ -6,6 +6,7 @@ import imaplib
 import base64
 import os
 import email
+import logging
 
 app = Flask(__name__)
 
@@ -28,83 +29,91 @@ class read_iridium(object):
         self.mail.login(self.username, self.password)
         self.mail.select('Inbox')
 
+        self.run_email_thread = False
+
     def connect(self):
         #start email thread
-        self.check_email_thread = threading.Thread(target=self.check_for_email)
+        #self.check_email_thread = threading.Thread(target=self.check_for_email)
+        self.check_email_thread = threading.Thread(target=self.testing)
         self.run_email_thread = True
         self.check_email_thread.start()
+
+    def testing(self):
+        while self.run_email_thread==True:
+            #print('This is an output', file=sys.stderr)
+            print("woah", flush=True)
 
     def process_downlink_packet(self, data):
         return json.loads(data)
 
     def check_for_email(self):
-        _, data = self.mail.search(None, '(FROM "fy56@cornell.edu")', '(UNSEEN)')
-        mail_ids = data[0]
-        id_list = mail_ids.split()
+        while self.run_email_thread==True:
+            _, data = self.mail.search(None, '(FROM "fy56@cornell.edu")', '(UNSEEN)')
+            mail_ids = data[0]
+            id_list = mail_ids.split()
 
-        #while self.run_email_thread==True:
-        for num in id_list:
-            #.fetch() fetches the mail for given id where 'RFC822' is an Internet 
-            # Message Access Protocol.
-            _, data = self.mail.fetch(num,'(RFC822)')
+            for num in id_list:
+                #.fetch() fetches the mail for given id where 'RFC822' is an Internet 
+                # Message Access Protocol.
+                _, data = self.mail.fetch(num,'(RFC822)')
 
-            #go through each component of data
-            for response_part in data:
-                if isinstance(response_part, tuple):
-                    # converts message from byte literal to string removing b''
-                    msg = email.message_from_string(response_part[1].decode('utf-8'))
-                    email_subject = msg['subject']
+                #go through each component of data
+                for response_part in data:
+                    if isinstance(response_part, tuple):
+                        # converts message from byte literal to string removing b''
+                        msg = email.message_from_string(response_part[1].decode('utf-8'))
+                        email_subject = msg['subject']
 
-                    #handles uplink confirmations
-                    if email_subject.find("SBD Mobile Terminated Message Queued for Unit: ")==0:
-                        for part in msg.walk():
-                                        
-                            if part.get_content_maintype() == 'multipart':
-                                continue
+                        #handles uplink confirmations
+                        if email_subject.find("SBD Mobile Terminated Message Queued for Unit: ")==0:
+                            for part in msg.walk():
+                                            
+                                if part.get_content_maintype() == 'multipart':
+                                    continue
 
-                            if part.get('Content-Disposition') is None:
-                                continue
-                                        
-                            #get the body text of the email and look for the MOMSN/MTMSN number
-                            if part.get_content_type() == "text/plain":
-                                email_body = part.get_payload(decode=True).decode('utf8')
-                                for line in email_body.splitlines():
-                                    if line.find("MTMSN")!=-1:
-                                        self.mtmsn=int(line[line.find("MTMSN")+9:line.find("MTMSN")+11])
+                                if part.get('Content-Disposition') is None:
+                                    continue
+                                            
+                                #get the body text of the email and look for the MOMSN/MTMSN number
+                                if part.get_content_type() == "text/plain":
+                                    email_body = part.get_payload(decode=True).decode('utf8')
+                                    for line in email_body.splitlines():
+                                        if line.find("MTMSN")!=-1:
+                                            self.mtmsn=int(line[line.find("MTMSN")+9:line.find("MTMSN")+11])
 
-                    #handles downlinks
-                    if 1==1:
-                    #if email_subject.find("SBD Msg From Unit: "==0:
-                        #go through the email contents
-                        for part in msg.walk():
-                                        
-                            if part.get_content_maintype() == 'multipart':
-                                continue
+                        #handles downlinks
+                        if 1==1:
+                        #if email_subject.find("SBD Msg From Unit: "==0:
+                            #go through the email contents
+                            for part in msg.walk():
+                                            
+                                if part.get_content_maintype() == 'multipart':
+                                    continue
 
-                            if part.get('Content-Disposition') is None:
-                                continue
-                                        
-                            #get the body text of the email and look for the MOMSN/MTMSN number
-                            if part.get_content_type() == "text/plain":
-                                email_body = part.get_payload(decode=True).decode('utf8')
-                                for line in email_body.splitlines():
-                                    if line.find("MOMSN")!=-1:
-                                        self.momsn=int(line[7:])
-                                    if line.find("MTMSN")!=-1:
-                                        self.confirmation_mtmsn=int(line[7:])
-                                        if self.confirmation_mtmsn != self.mtmsn:
-                                            #stop radio session from sending any more uplinks
-                                            self.send_uplinks=False
-                                        else:
-                                            #allow radio session to send more uplinks
-                                            self.send_uplinks=True
+                                if part.get('Content-Disposition') is None:
+                                    continue
+                                            
+                                #get the body text of the email and look for the MOMSN/MTMSN number
+                                if part.get_content_type() == "text/plain":
+                                    email_body = part.get_payload(decode=True).decode('utf8')
+                                    for line in email_body.splitlines():
+                                        if line.find("MOMSN")!=-1:
+                                            self.momsn=int(line[7:])
+                                        if line.find("MTMSN")!=-1:
+                                            self.confirmation_mtmsn=int(line[7:])
+                                            if self.confirmation_mtmsn != self.mtmsn:
+                                                #stop radio session from sending any more uplinks
+                                                self.send_uplinks=False
+                                            else:
+                                                #allow radio session to send more uplinks
+                                                self.send_uplinks=True
 
-                            #check if there is an email attachment
-                            if part.get_filename() is not None:
-                                #get data from email attachment
-                                attachmentContents=part.get_payload(decode=True).decode('utf8')
-                                data=self.process_downlink_packet(attachmentContents)
-                                return data
+                                #check if there is an email attachment
+                                if part.get_filename() is not None:
+                                    #get data from email attachment
+                                    attachmentContents=part.get_payload(decode=True).decode('utf8')
+                                    data=self.process_downlink_packet(attachmentContents)
+                                    r=requests.post(server,data)
 
     def disconnect(self):
         self.run_email_thread=False
@@ -130,18 +139,10 @@ auth = server_keys_config["auth"]
 #password = auth["password"]
 
 readIr=read_iridium(radio_keys_config, server_keys_config)
-readIr.connect()
 
-@app.route("/", methods=['POST', 'GET'])
+@app.route("/")
 def home():
-    while readIr.run_email_thread==True:
-        data=readIr.check_for_email()
-        if data is not None:
-            r=requests.post(server,data)
-            return r.text
-        if request.method=='POST':
-            return request
+    readIr.connect()
 
 if __name__ == "__main__":
-    #socketio.run(app)
     app.run(debug=True)
