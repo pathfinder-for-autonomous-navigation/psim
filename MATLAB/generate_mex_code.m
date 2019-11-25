27cc58bf27529619a38bef68e154410cd3dee4ef
nhz2 <nhz2@cornell.edu>
2019-11-24 20:36:58 -0500

    Fixed path issue in generated mex code

--- MATLAB/generate_mex_code.m
+++ MATLAB/generate_mex_code.m
@@ -1,2 +1,2 @@
-mex CXXFLAGS='$CXXFLAGS -std=c++14' ./environmental_models/helper_functions/geomag_wrapper.cpp -R2018a -silent
-mex CXXFLAGS='$CXXFLAGS -std=c++14' ./environmental_models/helper_functions/geograv_wrapper.cpp -R2018a -silent
+mex CXXFLAGS='$CXXFLAGS -std=c++14' ../MATLAB/environmental_models/helper_functions/geomag_wrapper.cpp -R2018a -silent
+mex CXXFLAGS='$CXXFLAGS -std=c++14' ../MATLAB/environmental_models/helper_functions/geograv_wrapper.cpp -R2018a -silent
