9acb5fbb333e652d5309de49b0447292e5ea3483
Fatima Yousuf <fatimayousuf@dhcp-clara-675.redrover.cornell.edu>
2019-12-05 21:27:21 -0500

    server continuously posts reports to elasticsearch

--- python/gsw/main.py
+++ python/gsw/main.py
@@ -13,6 +13,10 @@ app = Flask(__name__)
 
 class read_iridium(object):
     def __init__(self, radio_keys_config, server_keys_config):
+        #server
+        self.es_server=server_keys_config["server"]
+        self.es_port=server_keys_config["port"]
+
         #email
         self.username=radio_keys_config["email_username"]
         self.password=radio_keys_config["email_password"]
@@ -22,7 +26,8 @@ class read_iridium(object):
         self.mtmsn=-1
         self.confirmation_mtmsn=-1
 
-        #send_uplinks keeps track of if the ground can send more uplinks. This allows us to make sure we are only sending one uplink at a time.
+        #send_uplinks, keeps track of if the ground can send more uplinks. This allows us to make sure we are only sending one uplink at a time.
+        #need to move this function to radioSession
         self.send_uplinks=False
 
         #connect to email
@@ -30,11 +35,12 @@ class read_iridium(object):
         self.mail.login(self.username, self.password)
         self.mail.select('Inbox')
 
+        #thread
         self.run_email_thread = False
 
     def connect(self):
         #start email thread
-        self.check_email_thread = threading.Thread(target=self.check_for_email)
+        self.check_email_thread = threading.Thread(target=self.post_to_es)
         self.run_email_thread = True
         self.check_email_thread.start()
 
@@ -42,75 +48,101 @@ class read_iridium(object):
         return json.loads(data)
 
     def check_for_email(self):
-        if 1==1:
-        #while self.run_email_thread==True:
-            #look for all new emails from iridium
-            _, data = self.mail.search(None, '(FROM "fy56@cornell.edu")', '(UNSEEN)')
-            mail_ids = data[0]
-            id_list = mail_ids.split()
+        '''
+        Checks the PAN email account. Updates the most recent MOMSN 
+        and MTMSN numbers. Returns the contents of the email attachement
+        of the most recent unread email from the Iridium email account.
+        '''
+        #look for all new emails from iridium
+        _, data = self.mail.search(None, '(FROM "fy56@cornell.edu")', '(UNSEEN)')
+        mail_ids = data[0]
+        id_list = mail_ids.split()
 
-            for num in id_list:
-                #.fetch() fetches the mail for given id where 'RFC822' is an Internet 
-                # Message Access Protocol.
-                _, data = self.mail.fetch(num,'(RFC822)')
+        for num in id_list:
+            #.fetch() fetches the mail for given id where 'RFC822' is an Internet 
+            # Message Access Protocol.
+            _, data = self.mail.fetch(num,'(RFC822)')
 
-                #go through each component of data
-                for response_part in data:
-                    if isinstance(response_part, tuple):
-                        # converts message from byte literal to string removing b''
-                        msg = email.message_from_string(response_part[1].decode('utf-8'))
-                        email_subject = msg['subject']
+            #go through each component of data
+            for response_part in data:
+                if isinstance(response_part, tuple):
+                    # converts message from byte literal to string removing b''
+                    msg = email.message_from_string(response_part[1].decode('utf-8'))
+                    email_subject = msg['subject']
 
-                        #handles uplink confirmations
-                        if email_subject.find("SBD Mobile Terminated Message Queued for Unit: ")==0:
-                            for part in msg.walk():
+                    #handles uplink confirmations
+                    if email_subject.find("SBD Mobile Terminated Message Queued for Unit: ")==0:
+                        for part in msg.walk():
                                             
-                                if part.get_content_maintype() == 'multipart':
-                                    continue
+                            if part.get_content_maintype() == 'multipart':
+                                continue
 
-                                if part.get('Content-Disposition') is None:
-                                    continue
+                            if part.get('Content-Disposition') is None:
+                                continue
                                             
-                                #get the body text of the email and look for the MOMSN/MTMSN number
-                                if part.get_content_type() == "text/plain":
-                                    email_body = part.get_payload(decode=True).decode('utf8')
-                                    for line in email_body.splitlines():
-                                        if line.find("MTMSN")!=-1:
-                                            self.mtmsn=int(line[line.find("MTMSN")+9:line.find("MTMSN")+11])
+                            #get the body text of the email and look for the MOMSN/MTMSN number
+                            if part.get_content_type() == "text/plain":
+                                email_body = part.get_payload(decode=True).decode('utf8')
+                                for line in email_body.splitlines():
+                                    if line.find("MTMSN")!=-1:
+                                        self.mtmsn=int(line[line.find("MTMSN")+9:line.find("MTMSN")+11])
 
-                        #handles downlinks
-                        if 1==1:
-                        #if email_subject.find("SBD Msg From Unit: "==0:
-                            #go through the email contents
-                            for part in msg.walk():
+                    #handles downlinks
+                    if True:
+                    #if email_subject.find("SBD Msg From Unit: "==0:
+                        #go through the email contents
+                        for part in msg.walk():
+                                        
+                            if part.get_content_maintype() == 'multipart':
+                                continue
+
+                            if part.get('Content-Disposition') is None:
+                                continue
                                             
-                                if part.get_content_maintype() == 'multipart':
-                                    continue
+                            #get the body text of the email and look for the MOMSN/MTMSN number
+                            if part.get_content_type() == "text/plain":
+                                email_body = part.get_payload(decode=True).decode('utf8')
+                                for line in email_body.splitlines():
+                                    if line.find("MOMSN")!=-1:
+                                        self.momsn=int(line[7:])
+                                    if line.find("MTMSN")!=-1:
+                                        self.confirmation_mtmsn=int(line[7:])
+                                        if self.confirmation_mtmsn != self.mtmsn:
+                                            #stop radio session from sending any more uplinks
+                                            self.send_uplinks=False
+                                        else:
+                                            #allow radio session to send more uplinks
+                                            self.send_uplinks=True
 
-                                if part.get('Content-Disposition') is None:
-                                    continue
-                                            
-                                #get the body text of the email and look for the MOMSN/MTMSN number
-                                if part.get_content_type() == "text/plain":
-                                    email_body = part.get_payload(decode=True).decode('utf8')
-                                    for line in email_body.splitlines():
-                                        if line.find("MOMSN")!=-1:
-                                            self.momsn=int(line[7:])
-                                        if line.find("MTMSN")!=-1:
-                                            self.confirmation_mtmsn=int(line[7:])
-                                            if self.confirmation_mtmsn != self.mtmsn:
-                                                #stop radio session from sending any more uplinks
-                                                self.send_uplinks=False
-                                            else:
-                                                #allow radio session to send more uplinks
-                                                self.send_uplinks=True
+                            #check if there is an email attachment
+                            if part.get_filename() is not None:
+                                #get data from email attachment
+                                attachmentContents=part.get_payload(decode=True).decode('utf8')
+                                statefield_report=self.process_downlink_packet(attachmentContents)
+                                return statefield_report
 
-                                #check if there is an email attachment
-                                if part.get_filename() is not None:
-                                    #get data from email attachment
-                                    attachmentContents=part.get_payload(decode=True).decode('utf8')
-                                    statefield_report=self.process_downlink_packet(attachmentContents)
-                                    return statefield_report
+    def post_to_es(self):
+        '''
+        Check for the most recent email from iridium.
+        If there is a statefield report, index the statefield report and 
+        create and index an iridium report.
+        '''
+        while self.run_email_thread==True:
+            #connect to elasticsearch
+            es=Elasticsearch([{'host':self.es_server,'port':self.es_port}])
+
+            sf_report=readIr.check_for_email()
+            if sf_report is not None:
+                #index statefield report in elasticsearch
+                statefield_res = es.index(index='statefield_report', doc_type='report', body=sf_report)
+
+                # Create an iridium report and add that to the iridium_report index in elasticsearch. 
+                # We only want to add Iridium reports when we recieve an email, which is why we wait until there is a statefield report
+                ir_report=json.dumps({"momsn":readIr.momsn, "mtmsn":readIr.mtmsn})
+                iridium_res = es.index(index='iridium_report', doc_type='report', body=ir_report)
+
+                #print whether or not indexing was successful
+                print(statefield_res['result']+"\n"+iridium_res['result'])
 
     def disconnect(self):
         self.run_email_thread=False
@@ -128,48 +160,34 @@ except json.JSONDecodeError:
 except KeyError:
     print("Malformed config file. Exiting.")
     raise SystemExit
-username=server_keys_config["username"]
-password=server_keys_config["password"]
-es_server=server_keys_config["server"]
-es_port=server_keys_config["port"]
 
-#create a read_iridium object 
+#create a read_iridium object
 readIr=read_iridium(radio_keys_config, server_keys_config)
-
-@app.route("/")
-def home():
-    #get token for session. honestly doesn't seem like we even need this, but we shall see?
-    '''
-    headers = {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json',
-    }
-    login={
-        "username":username,
-        "password":password
-    }
-    login_json=json.dumps(login)
-    response = requests.post('http://'+readIr.server+":"+readIr.port+"/auth/login", headers=headers, data=login_json)
-    response_body=json.loads(response.text)
-    token=response_body["token"]
-    '''
-
-    #connect to elasticsearch
-    es=Elasticsearch([{'host':es_server,'port':es_port}])
-
-    #check email and post data to elasticsearch
-    sf_report=readIr.check_for_email()
-    if sf_report is not None:
-        statefield_res = es.index(index='statefield_report', doc_type='report', body=sf_report)
-        # Create an iridium report and add that to the Iridium report index in elasticsearch. 
-        # We only want to add Iridium reports when we recieve an email, which is why we wait until there is a statefield report
-        ir_report={"momsn":readIr.momsn, "mtmsn":readIr.mtmsn}
-        iridium_res = es.index(index='iridium_report', doc_type='report', body=json.dumps(ir_report))
-        return statefield_res['result']+"\n"+iridium_res['result']
-    else:
-        return "no email found"
-
-
+#start checking emails and posting reports
+readIr.connect()
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+
+
+
+
+#gets token for session. honestly doesn't seem like we even need this, but we shall see?
+'''
+headers = {
+    'Content-Type': 'application/json',
+    'Accept': 'application/json',
+}
+login={
+    "username":username,
+    "password":password
+}
+login_json=json.dumps(login)
+response = requests.post('http://'+readIr.server+":"+readIr.port+"/auth/login", headers=headers, data=login_json)
+response_body=json.loads(response.text)
+token=response_body["token"]
+'''
