96e6a195f7aa1ac3a2a9bf1bbf3d7df6a980b9b2
Tanishq Aggarwal <tanishq.aggarwal.11@gmail.com>
2019-12-26 14:52:41 -0500

    Minor updates to CI infrastructure and gitignores (#101)
    * Minor updates to CI infrastructure and gitignores
    
    * Bugfix for GNC utilities
    
    
    Former-commit-id: 93a7ec195fe99325d24040a1d10adb212816eb9c
    Former-commit-id: 6875373dd3e7fa5c0b95fc6fd214df938901e5e7

---
+++ src/main.cpp
@@ -0,0 +1,23 @@
+// Stub file to compile with PlatformIO.
+
+// Add includes to ensure they don't break compilation
+#include <gnc_attitude_estimation.hpp>
+#include <gnc_constants.hpp>
+#include <gnc_containers.hpp>
+#include <gnc_environment.hpp>
+#include <gnc_ode.hpp>
+#include <gnc_utilities.hpp>
+#include <lin.hpp>
+
+#ifndef UNIT_TEST
+#ifdef TEST_DESKTOP
+    int main() {
+        return 0;
+    }
+#else
+    #include <Arduino.h>
+
+    void setup() {}
+    void loop()  {}
+#endif
+#endif
