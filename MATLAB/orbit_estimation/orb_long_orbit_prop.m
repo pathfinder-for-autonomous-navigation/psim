df1f86e73c7d24671304f1cca314e230465975d4
nhz2 <nhz2@cornell.edu>
2020-01-31 01:05:29 -0500

    Update orb_long_orbit_prop.m

--- MATLAB/orbit_estimation/orb_long_orbit_prop.m
+++ MATLAB/orbit_estimation/orb_long_orbit_prop.m
@@ -1,44 +1,30 @@
-function [r_ecef,v_ecef,jacobian,rel_target_r_ecef,rel_target_v_ecef,target_jacobian] = orb_short_orbit_prop(r_ecef,v_ecef,rel_target_r_ecef,rel_target_v_ecef,dt,start_time)
-%orb_short_orbit_prop Updates orbit, and gets the jacbian.
-%   note the target is relative just to avoid floating pointing errors, for
-%   the jacobians, they are as if the target is completely seperate.
+function [r_ecef,v_ecef] = orb_long_orbit_prop(r_ecef,v_ecef,duration,start_time)
+%orb_short_orbit_prop Updates orbit
 %   The orbit propigator is designed for nearly circular low earth orbit, and
 %   ignores all forces except gravity from env_gravity.
+%   Using higher order integrators from 
+%   https://doi.org/10.1016/0375-9601(90)90092-3
 %
-%   The jacobians are calculated assuming point mass earth.
-%   
 %   r_ecef(matrix (3,1)):
 %       position in ecef coordinate system (m)
 %   v_ecef(matrix (3,1)):
 %       velocity in ecef coordinate system (m/s)
-%   jacobian(matrix (6,6)):
-%       Approximate jacobian of the tranform from [r_ecef;v_ecef;] input to
-%       [r_ecef;v_ecef;] output.
-%   rel_target_r_ecef(matrix (3,1)):
-%       position of the target relative to r_ecef in ecef coordinate system (m)
-%   rel_target_v_ecef(matrix (3,1)):
-%       velocity of the target relative to v_ecef in ecef coordinate system (m/s)
-%   target_jacobian(matrix (6,6)):
-%       Approximate jacobian of the tranform from [rel_target_r_ecef+r_ecef;rel_target_v_ecef+v_ecef;] input to
-%       [rel_target_r_ecef+r_ecef;rel_target_v_ecef+v_ecef;] output.
-%   dt(double -0.2 to 0.2):
+%   dt(double):
 %       Time step, how much time to update the orbit (s)
 %   start_time(double): seconds since const.INITGPS_WN
 %
-% Started by Nathan Zimmerberg on Jan 29, 2020
+% Started by Nathan Zimmerberg on Jan 30, 2020
 % Authors: Nathan Zimmerberg (nhz2@cornell.edu)
-% Latest Revision: Jan 29, 2020
+% Latest Revision: Jan 30, 2020
 % Pathfinder for Autonomous Navigation
 % Space Systems Design Studio
 % Cornell University
 global const
 
-%% step 1a ecef->ecef0
+%% ecef->ecef0
 r_ecef0= r_ecef;
 v_ecef0= v_ecef+cross(const.earth_rate_ecef,r_ecef);
-rel_target_r_ecef0= rel_target_r_ecef;
-rel_target_v_ecef0= rel_target_v_ecef+cross(const.earth_rate_ecef,rel_target_r_ecef);
-%% step 1b get circular orbit reference
+%% get circular orbit reference
 energy= 0.5*dot(v_ecef0,v_ecef0)-const.mu/norm(r_ecef0);
 a=-const.mu/2/energy;
 h_ecef0= cross(r_ecef0,v_ecef0);
@@ -47,52 +33,84 @@ x=x/norm(x)*a;
 y= cross(h_ecef0,r_ecef0);
 y=y/norm(y)*a;
 omega= h_ecef0/norm(h_ecef0)*sqrt(const.mu/(a*a*a));
-[orb_r,orb_v]= circular_orbit(0,0,omega,x,y);
+t0=0;
+[orb_r,orb_v]= circular_orbit(0,t0,omega,x,y);
 rel_r= r_ecef0-orb_r;
 rel_v= v_ecef0-orb_v;
-%% step 2 drift
-rel_r= rel_r+rel_v*dt*0.5;
-rel_target_r_ecef0= rel_target_r_ecef0+rel_target_v_ecef0*dt*0.5;
-%% step 3a calc acceleration at the half step
-t= 0.5*dt;
-[orb_r,~]= circular_orbit(t,0,omega,x,y);
-r_ecef0= rel_r+orb_r;
-target_pos_ecef0= r_ecef0+rel_target_r_ecef0;
-r_half_ecef0= r_ecef0;
-target_pos_half_ecef0= target_pos_ecef0;
-now = start_time + t; %current time in seconds
-dcm_ecef_ecef0 = relative_earth_dcm(t);
-pos_ecef=dcm_ecef_ecef0*r_ecef0;
-target_pos_ecef=dcm_ecef_ecef0*target_pos_ecef0;
-% perturbations due to J-coefficients; returns acceleration in ECEF
-g_ecef=env_gravity(now,pos_ecef);
-target_g_ecef=env_gravity(now,target_pos_ecef)-g_ecef;
-%convert to ECEF0
-acc =dcm_ecef_ecef0'*g_ecef+const.mu*orb_r/a^3;
-target_acc =dcm_ecef_ecef0'*target_g_ecef;
-%% step 3b kick velocity
-rel_v= rel_v + acc*dt*1;
-rel_target_v_ecef0= rel_target_v_ecef0+target_acc*dt;
-%% step 4 drift
-rel_r= rel_r+rel_v*dt*0.5;
-rel_target_r_ecef0= rel_target_r_ecef0+rel_target_v_ecef0*dt*0.5;
-%% step 5a get back absolute orbit
-[orb_r,orb_v]= circular_orbit(dt,0,omega,x,y);
-r_ecef0= rel_r+orb_r;
-v_ecef0= rel_v+orb_v;
-%% step 5b rotate back to ecef
+%% get higher order integrator steps
+%high order integrators
+%https://doi.org/10.1016/0375-9601(90)90092-3
+%https://pdf.sciencedirectassets.com/271541/1-s2.0-S0375960100X05075/1-s2.0-0375960190900923/main.pdf?X-Amz-Security-Token=IQoJb3JpZ2luX2VjEHIaCXVzLWVhc3QtMSJGMEQCIF1ECb38EYCxayPQRI0hpkbfjJc8eQ%2FAlwjL8FEtSVN1AiAg%2BNndqK38eCFIGiNiIh1DajL%2FBlIVrQFa6idjCf4l9Cq9Awj7%2F%2F%2F%2F%2F%2F%2F%2F%2F%2F8BEAIaDDA1OTAwMzU0Njg2NSIMsEfyy6bWwA0G0iHLKpEDA%2FBHuVZ%2BVrJnY6l8heiEZ7jxtjL2zdPELqgIjgiJUo50nM%2FMAOinJx82WVIbukNl56aGWSCe6Pysdn2mS9cQx%2BrF2HAoTs%2BpiBadw%2FuAtggU4RQLc9%2Ffu4by%2FaCZYOYkTV7dA8KYAibKr76LaKZmZcal%2B2iHVc6pZom5iof3O%2FIMV%2BYiKuxkGKv9aiztBwn59PsE0v4odCDkbfs%2FNeIzNNjvZOqMd84xXSKB%2FND4TJArkhpBBo%2F9DShQp4wBwZepjKHaDAlmSJbyXt1nvlkr2c55HSXYMFzA0HocbScbekInnxoVRRIBhSlAJqVKLeTKScO%2B3%2BEuIJM9i3Mdf%2FZF4LfNdQad1Edalc6Xg7seo1Cm13aX92SIbBql8JIHQrhL%2F8YO4CNgtv6f4ajHqprlsq4Dbd6hVeM%2F%2F9pO7cyBibdRZMFDFKj%2Bn1yNx6voOQna4fXd9953vNwVer4TbZEMuxAI3gyte7V9uxc4qqi33wYavVFpBeHv8%2FAp1u5pE0h4NvB7SCTJ2VFEl7m6mKN5mIQwsLSZ8QU67AFWLmQMZ2r2Bx6Eos9Sx7Q9MQju0X5Dacz5rMWai7MeWLv4cbY05t%2BFzppKCGLTEE%2B80u6E59onOWV1ppmvgYYD%2BH%2FxMskaYil9mZuhhfSkrVbTEbUmyWIURFu76ht72ZGzfLiuw35ilknpQoeewBNIGwvEn%2FWbHsv1vrmn0JZk2TNVZBDaS7jEXpJINs7%2BJKo%2F82ngZKaGLwMLH%2BDUrbdd%2B0WqjiKX6y%2BAbBVpLjlZsRj%2BPoV2d97UR6ABtYSVHv%2FWITdO7Wd930KQQizMrh3yZDDDUrj4UQAqWpqqZv38OQ2HvDHcvhvx4wowyA%3D%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20200121T024645Z&X-Amz-SignedHeaders=host&X-Amz-Expires=300&X-Amz-Credential=ASIAQ3PHCVTYSQKDTZ5H%2F20200121%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=087c847babff243b457807c797f220e3743de6d63902d0943a61cd30e30ee95b&hash=4a586ea181fec35778b5f7e60a625cdd055f00d9ea16572cef4a41192adaebc9&host=68042c943591013ac2b2430a89b270f6af2c76d8dfd086a07176afe7c76c2c61&pii=0375960190900923&tid=spdf-306995ff-861f-40bd-9c97-a080a5d8c1b2&sid=0f7242a41192874f6a6a8bb-6ac9f383dac9gxrqa&type=client      
+w6a= [-0.117767998417887E1,
+    0.235573213359357E0,
+    0.784513610477560E0,];
+w=w6a;
+m=length(w);
+N= max(round(abs(duration)/15/(m*2+1)),1);
+w0= 1-2*sum(w);
+c=zeros(1,2*m+2);
+d=zeros(1,2*m+1);
+for i= 2:m
+    c(i)= 0.5*(w(m+2-i)+w(m+1-i));
+    c(2*m+3-i)= 0.5*(w(m+2-i)+w(m+1-i));
+end
+c(1)= 0.5*w(m);
+c(2*m+2)= 0.5*w(m);
+c(m+1)= 0.5*(w(1)+w0);
+c(m+2)= 0.5*(w(1)+w0);
+for i= 1:m
+    d(i)= w(m+1-i);
+    d(2*m+2-i)= w(m+1-i);
+end
+d(m+1)=w0;
+dt= duration/(N);
+%% Iterations
+for i=1:N
+    t= (i-1)*dt;
+    for j= 1:length(d)
+        % drift positions dt*c(j)
+        rel_r= rel_r+rel_v*dt*c(j);
+        t= t+dt*c(j);
+        % calc acceleration at the half step
+        [orb_r,~]= circular_orbit(t,t0,omega,x,y);
+        now = start_time + t; %current time in seconds
+        dcm_ecef_ecef0 = relative_earth_dcm(t);
+        pos_ecef= dcm_ecef_ecef0*(orb_r+rel_r);
+        % perturbations due to J-coefficients; returns acceleration in ECEF
+        g_ecef=env_gravity(now,pos_ecef);
+        %convert to ECEF0
+        acc= dcm_ecef_ecef0'*g_ecef+const.mu*orb_r/a^3;
+        % kick velocity dt*d(j)
+        rel_v= rel_v + acc*dt*d(j);
+    end
+    % final drift positions dt*c(end)
+    rel_r= rel_r+rel_v*dt*c(end);
+    % reset refernece orbit
+    [r_ecef0,v_ecef0]= circular_orbit(i*dt,t0,omega,x,y);
+    r_ecef0= rel_r+r_ecef0;
+    v_ecef0= rel_v+v_ecef0;
+    energy= 0.5*dot(v_ecef0,v_ecef0)-const.mu/norm(r_ecef0);
+    a=-const.mu/2/energy;
+    h_ecef0= cross(r_ecef0,v_ecef0);
+    x= r_ecef0;
+    x=x/norm(x)*a;
+    y= cross(h_ecef0,r_ecef0);
+    y=y/norm(y)*a;
+    omega= h_ecef0/norm(h_ecef0)*sqrt(const.mu/(a*a*a));
+    t0=i*dt;
+    [orb_r,orb_v]= circular_orbit(t0,t0,omega,x,y);
+    rel_r= r_ecef0-orb_r;
+    rel_v= v_ecef0-orb_v;
+end
+%% Get back absolute orbit
+[r_ecef0,v_ecef0]= circular_orbit(duration,t0,omega,x,y);
+r_ecef0= rel_r+r_ecef0;
+v_ecef0= rel_v+v_ecef0;
+%% ecef0->ecef
 dcm_ecef_ecef0 = relative_earth_dcm(dt);
 r_ecef= dcm_ecef_ecef0*r_ecef0;
 v_ecef= dcm_ecef_ecef0*v_ecef0;
-rel_target_r_ecef= dcm_ecef_ecef0*rel_target_r_ecef0;
-rel_target_v_ecef= dcm_ecef_ecef0*rel_target_v_ecef0;
-%% step 6 remove cross r term from velocity
 v_ecef= v_ecef-cross(const.earth_rate_ecef,r_ecef);
-rel_target_v_ecef= rel_target_v_ecef-cross(const.earth_rate_ecef,rel_target_r_ecef);
-
-%% compute jacobians
-jacobian= get_jacobian(r_half_ecef0,dcm_ecef_ecef0,dt);
-target_jacobian= get_jacobian(target_pos_half_ecef0,dcm_ecef_ecef0,dt);
 end
 
 function [r,v]= circular_orbit(t,t0,omega,x,y)
@@ -118,35 +136,4 @@ function M = cross_product_matrix(x)
     M=[ 0 -x(3) x(2);
         x(3) 0 -x(1);
         -x(2) x(1) 0;];
-end
-
-function H = hessian_helper(x)
-    %returns the hessian of gravity, mks units
-    %from equation (3.154) in Fundamentals of Spacecraft Attitude Determination and Control
-    global const
-    r= norm(x);
-    H= -const.mu/r^3*(eye(3)-3*(x*x')/r^2);
-end
-
-function [J]= get_jacobian(r_half_ecef0,dcm_ecef_ecef0,dt)
-    %returns the jacobians
-    %target_r_half_ecef0 and r_half_ecef0 are to position vetors on the
-    %half step mks units, dt is the time step.
-    %dcm_ecef_ecef0 is the dcm to rotate from initial ecef to ecef
-    %dt seconds latter.
-    global const
-    wx= cross_product_matrix(const.earth_rate_ecef);
-    H= hessian_helper(r_half_ecef0);
-    A= dcm_ecef_ecef0;
-    Hdt= H*dt;
-    I= eye(3);
-    iwx= I+dt*0.5*wx;
-    hiwx= Hdt*iwx;
-    rr41= I+dt*wx+hiwx*dt*0.5;
-    rv41= dt*I+Hdt*dt*dt*0.25;
-    vr41= hiwx+wx;
-    vv41= I+Hdt*dt*0.5;
-    wxtA= wx'*A;
-    J= [A*rr41 A*rv41;
-        wxtA*rr41+A*vr41 A*vv41+wxtA*rv41;];
 end
