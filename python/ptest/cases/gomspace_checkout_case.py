feaacff21a46617c1bd2ec4d429341989c42e566
Andreea Foarce <aaf68@cornell.edu>
2020-02-08 17:33:28 -0500

    Added conditions for battery and outputs.
    Former-commit-id: 4f3fe06a29cc8eeddf1dcfad398dcef1defe8099
    Former-commit-id: d02f171c96272b155f931bc7e2520698aebcb7d3

--- python/ptest/cases/gomspace_checkout_case.py
+++ python/ptest/cases/gomspace_checkout_case.py
@@ -17,45 +17,62 @@ class GomspaceCheckoutCase(SingleSatOnlyCase):
 
         # readable fields
 
-        simulation.flight_controller.read_state("gomspace.vboost.output1")
-        simulation.flight_controller.read_state("gomspace.vboost.output2")
-        simulation.flight_controller.read_state("gomspace.vboost.output3")
+        vboost = [
+            simulation.flight_controller.read_state("gomspace.vboost.output1"),
+            simulation.flight_controller.read_state("gomspace.vboost.output2"),
+            simulation.flight_controller.read_state("gomspace.vboost.output3")]
 
-        simulation.flight_controller.read_state("gomspace.vbatt")
+        vbatt = simulation.flight_controller.read_state("gomspace.vbatt")
+        if vbatt < 6.0 or vbatt > 8.4:
+            print("Vbatt is out of expected range [6, 8.4] at: " + vbatt)
 
-        simulation.flight_controller.read_state("gomspace.curin.output1")
-        simulation.flight_controller.read_state("gomspace.curin.output2")
-        simulation.flight_controller.read_state("gomspace.curin.output3")
+        currin = [
+            simulation.flight_controller.read_state("gomspace.curin.output1"),
+            simulation.flight_controller.read_state("gomspace.curin.output2"),
+            simulation.flight_controller.read_state("gomspace.curin.output3")]
 
-        simulation.flight_controller.read_state("gomspace.cursun")
-        simulation.flight_controller.read_state("gomspace.cursys")
+        currsun = simulation.flight_controller.read_state("gomspace.cursun")
+        cursys = simulation.flight_controller.read_state("gomspace.cursys")
 
-        simulation.flight_controller.read_state("gomspace.curout.output1")
-        simulation.flight_controller.read_state("gomspace.curout.output2")
-        simulation.flight_controller.read_state("gomspace.curout.output3")
-        simulation.flight_controller.read_state("gomspace.curout.output4")
-        simulation.flight_controller.read_state("gomspace.curout.output5")
-        simulation.flight_controller.read_state("gomspace.curout.output6")
+        curout = [
+            simulation.flight_controller.read_state("gomspace.curout.output1"),
+            simulation.flight_controller.read_state("gomspace.curout.output2"),
+            simulation.flight_controller.read_state("gomspace.curout.output3"),
+            simulation.flight_controller.read_state("gomspace.curout.output4"),
+            simulation.flight_controller.read_state("gomspace.curout.output5"),
+            simulation.flight_controller.read_state("gomspace.curout.output6")]
 
-        simulation.flight_controller.read_state("gomspace.output.output1")
-        simulation.flight_controller.read_state("gomspace.output.output2")
-        simulation.flight_controller.read_state("gomspace.output.output3")
-        simulation.flight_controller.read_state("gomspace.output.output4")
-        simulation.flight_controller.read_state("gomspace.output.output5")
-        simulation.flight_controller.read_state("gomspace.output.output6")
+        output = [
+            simulation.flight_controller.read_state("gomspace.output.output1"),
+            simulation.flight_controller.read_state("gomspace.output.output2"),
+            simulation.flight_controller.read_state("gomspace.output.output3"),
+            simulation.flight_controller.read_state("gomspace.output.output4"),
+            simulation.flight_controller.read_state("gomspace.output.output5"),
+            simulation.flight_controller.read_state("gomspace.output.output6")]
 
-        simulation.flight_controller.read_state("gomspace.wdt_i2c_time_left")
-        simulation.flight_controller.read_state("gomspace.counter_wdt_i2c")
-        simulation.flight_controller.read_state("gomspace.counter_boot")
+        for n in range(0, len(output)):
+            out_n = output[n]
+            if out_n < 0.5 or out_n > 3.0:
+                print("Output-" + n +
+                      " is out of range [0.3, 0.5] at: " + out_n)
 
-        simulation.flight_controller.read_state("gomspace.temp.output1")
-        simulation.flight_controller.read_state("gomspace.temp.output2")
-        simulation.flight_controller.read_state("gomspace.temp.output3")
-        simulation.flight_controller.read_state("gomspace.temp.output4")
+        wdt_i2c_time_left = simulation.flight_controller.read_state(
+            "gomspace.wdt_i2c_time_left")
+        counter_wdt_i2c = simulation.flight_controller.read_state(
+            "gomspace.counter_wdt_i2c")
+        counter_boot = simulation.flight_controller.read_state(
+            "gomspace.counter_boot")
 
-        simulation.flight_controller.read_state("gomspace.bootcause")
-        simulation.flight_controller.read_state("gomspace.battmode")
-        simulation.flight_controller.read_state("gomspace.pptmode")
+        temp = [
+            simulation.flight_controller.read_state("gomspace.temp.output1"),
+            simulation.flight_controller.read_state("gomspace.temp.output2"),
+            simulation.flight_controller.read_state("gomspace.temp.output3"),
+            simulation.flight_controller.read_state("gomspace.temp.output4")]
+
+        bootcause = simulation.flight_controller.read_state(
+            "gomspace.bootcause")
+        battmode = simulation.flight_controller.read_state("gomspace.battmode")
+        pptmode = simulation.flight_controller.read_state("gomspace.pptmode")
 
         # writable fields
 
