c32e375728abd789335e736280facd0832e0e107
nhz2 <nhz2@cornell.edu>
2020-02-09 16:06:26 -0500

    added noise
    Former-commit-id: 3090086d647ebac4b6c16e36465d0d40945a6dbe [formerly 4aae3d63684f4ba6353ada3a418b4659f95d88f3]
    Former-commit-id: 0ec5f00cd3f474763439fad2a3e537c20634c32b
    Former-commit-id: 95938dffbd089cf2d8202a862a9ab696ffd0ee62

--- MATLAB/rendezvous/two_rendezvous_J2.m
+++ MATLAB/rendezvous/two_rendezvous_J2.m
@@ -9,6 +9,7 @@ V_rel   = 0.6; % Relative velocity at deployment (m/s)
 t_drift = 4.0 * 90.0 * 60.0; % Drift time (s)
 p = 0.7E-2;
 d = 1;
+thrust_noise_ratio = 0.1;
 
 a  = 6793137.0;  % Semimajor axis                        (m)
 e  = 0.001;        % Eccentricity                          (unitless)
@@ -45,15 +46,19 @@ y_dvs=zeros(N,1);
 deltaenergies=zeros(N,1);
 for i = 1:(N - 1)
     i
+    r1_measure=r1+0.1*randn(3,1);
+    r2_measure=r2+0.1*randn(3,1);
+    v1_measure=v1+0.001*randn(3,1);
+    v2_measure=v2+0.001*randn(3,1);
     % Calculate our hill state
-    Q_eci_hill = utl_eci2hill(r1, v1);
-    r_hill = Q_eci_hill * (r2 - r1);
-    v_hill = Q_eci_hill * (v2 - v1) - cross(w_hill, r_hill);
+    Q_eci_hill = utl_eci2hill(r1_measure, v1_measure);
+    r_hill = Q_eci_hill * (r2_measure - r1_measure);
+    v_hill = Q_eci_hill * (v2_measure - v1_measure) - cross(w_hill, r_hill);
     %point B
-    [~,potential,~]= env_gravity(0,r1);
-    energy1= 0.5*dot(v1,v1)-potential;
-    [~,potential,~]= env_gravity(0,r2);
-    energy2= 0.5*dot(v2,v2)-potential;
+    [~,potential,~]= env_gravity(0,r1_measure);
+    energy1= 0.5*dot(v1_measure,v1_measure)-potential;
+    [~,potential,~]= env_gravity(0,r2_measure);
+    energy2= 0.5*dot(v2_measure,v2_measure)-potential;
     if (norm(r_hill(2))<10E3 && abs(energy1-energy2)<200)
         %do cw
         x_dv=-v_hill(1);
@@ -62,33 +67,38 @@ for i = 1:(N - 1)
         y_maxu= 1/n*6*pi*sqrt(2)/2*max_dv_thrust;
         y_u= discrete_suicide_burn(y_maxu, y_v,y_x);
         y_dv= y_u/(-1/n*6*pi);
-        dv=y_dv*v2/norm(v2);
+        dv=y_dv*v2_measure/norm(v2_measure);
     else
         %just use energy pd
         x_dv=0;
         %zero energy diff pd control
         pterm= p*r_hill(2)
         dterm= d*(energy1-energy2)
-        dv=(pterm+dterm)*v2;
+        dv=(pterm+dterm)*v2_measure;
         dv= max(min(dv,sqrt(2)/2*max_dv_thrust),-sqrt(2)/2*max_dv_thrust);
     end
     u_now_hill= [x_dv;0;0;];
     % Apply actuation
-    dv= Q_eci_hill' * u_now_hill + dv + normal_plane_correction(r1,v1,r2,v2,max_dv_thrust);
+    dv= Q_eci_hill' * u_now_hill + dv + normal_plane_correction(r1_measure,v1_measure,r2_measure,v2_measure,max_dv_thrust);
     dv= dv/norm(dv)*min(norm(dv),max_dv_thrust);
+    dv= dv+thrust_noise_ratio*norm(dv)*randn(3,1);
     v2 = v2 + dv;
     % Simulate dynamics
     dt_fire= 3*pi/2/n;
     [~, r1, v1, r2, v2] = drift_phase(r1, v1, r2, v2, dt_fire);
+    r1_measure=r1+0.1*randn(3,1);
+    r2_measure=r2+0.1*randn(3,1);
+    v1_measure=v1+0.001*randn(3,1);
+    v2_measure=v2+0.001*randn(3,1);
     % Calculate our hill state
-    Q_eci_hill = utl_eci2hill(r1, v1);
-    r_hill = Q_eci_hill * (r2 - r1);
-    v_hill = Q_eci_hill * (v2 - v1) - cross(w_hill, r_hill);
+    Q_eci_hill = utl_eci2hill(r1_measure, v1_measure);
+    r_hill = Q_eci_hill * (r2_measure - r1_measure);
+    v_hill = Q_eci_hill * (v2_measure - v1_measure) - cross(w_hill, r_hill);
     %point A
-    [~,potential,~]= env_gravity(0,r1);
-    energy1= 0.5*dot(v1,v1)-potential;
-    [~,potential,~]= env_gravity(0,r2);
-    energy2= 0.5*dot(v2,v2)-potential;
+    [~,potential,~]= env_gravity(0,r1_measure);
+    energy1= 0.5*dot(v1_measure,v1_measure)-potential;
+    [~,potential,~]= env_gravity(0,r2_measure);
+    energy2= 0.5*dot(v2_measure,v2_measure)-potential;
     if (norm(r_hill(2))<10E3 && abs(energy1-energy2)<200)
         %do cw
         x_dv= -n*(4*r_hill(1)+1/n*v_hill(1)+2/n*v_hill(2));
@@ -101,13 +111,14 @@ for i = 1:(N - 1)
         %zero energy diff pd control
         pterm= p*r_hill(2)
         dterm= d*(energy1-energy2)
-        dv=(pterm+dterm)*v2;
+        dv=(pterm+dterm)*v2_measure;
         dv= max(min(dv,sqrt(2)/2*max_dv_thrust),-sqrt(2)/2*max_dv_thrust);
     end
     u_now_hill= [x_dv;0;0];
     % Apply actuation
-    dv= Q_eci_hill' * u_now_hill+dv+normal_plane_correction(r1,v1,r2,v2,max_dv_thrust);
+    dv= Q_eci_hill' * u_now_hill+dv+normal_plane_correction(r1_measure,v1_measure,r2_measure,v2_measure,max_dv_thrust);
     dv= dv/norm(dv)*min(norm(dv),max_dv_thrust);
+    dv= dv+thrust_noise_ratio*norm(dv)*randn(3,1);
     v2 = v2 + dv;
     % Simulate dynamics
     dt_fire= pi/2/n;
