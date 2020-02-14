8042d1bc347c4dc1876ba74b9566720ebf777331
Andreea Foarce <aaf68@cornell.edu>
2020-02-14 18:38:49 -0500

    Bug Fixes
    Checked umbilical for output values,
    Added cases to test writable fields,
    casted strings
    
    
    Former-commit-id: 239745ee5a42acc4cee84e71d52afb8f2a61e44a [formerly c124bd25cabefe718b283ebcf50b10b9659a722e]
    Former-commit-id: 7ac06c09c36063a809b0bfcb4a0afe7608032ac9
    Former-commit-id: e086f801295da59b9889b901655aa1aaea99efb4

--- python/ptest/cases/gomspace_checkout_case.py
+++ python/ptest/cases/gomspace_checkout_case.py
@@ -24,53 +24,58 @@ class GomspaceCheckoutCase(SingleSatOnlyCase):
 
         # readable fields
 
-        vboost = [read_state(self, "gomspace.vboost.output" + str(i))
+        vboost = [int(read_state(self, "gomspace.vboost.output" + str(i)))
                   for i in range(1, 4)]
+        for n in range(0, len(vboost)):
+            print("vboost" + str(n) + " is: " + str(vboost[n]) + " mV")
 
-        vbatt = simulation.flight_controller.read_state("gomspace.vbatt")
+        vbatt = int(read_state(self, "gomspace.vbatt"))
         if vbatt < 6000 or vbatt > 8400:
             print(
                 "Vbatt is out of expected range [6000, 8400] mV at: " + str(vbatt))
 
-        curin = [read_state(self, "gomspace.curin.output" + str(i))
+        curin = [int(read_state(self, "gomspace.curin.output" + str(i)))
                  for i in range(1, 4)]
+        for n in range(0, len(curin)):
+            print("curin" + str(n) + " is: " + str(curin[n]) + " mA")
+        cursun = int(read_state(self, "gomspace.cursun"))
+        print("cursun is: " + str(cursun) + " mA")
+        cursys = int(read_state(self, "gomspace.cursys"))
+        print("cursys is: " + str(cursys) + " mA")
 
-        cursun = read_state(self, "gomspace.cursun")
-        cursys = read_state(self, "gomspace.cursys")
-
-        curout = [read_state(self, "gomspace.curout.output" + str(i))
+        curout = [int(read_state(self, "gomspace.curout.output" + str(i)))
                   for i in range(1, 7)]
+        for n in range(0, len(curout)):
+            print("curout" + str(n) + " is: " + str(curout[n]) + " mA")
 
-        output = [read_state(self, "gomspace.output.output" + str(i))
+        output = [bool(read_state(self, "gomspace.output.output" + str(i)))
                   for i in range(1, 7)]
 
         for n in range(0, len(output)):
             out_n = output[n]
-            # TODO: check umbilical for which outputs should be 0 and 1
-            if out_n < 0.5 or out_n > 3.0:
+            # checked umbilical for which outputs should be 0 and 1:
+            # 1-5 are all 5V, 6 is 3.3V
+            if out_n is False:
                 print("Output-" + n +
-                      " is out of range [0.3, 0.5] at: " + out_n)
+                      " is not on: ")
 
-        wdt_i2c_time_left = simulation.flight_controller.read_state(
-            "gomspace.wdt_i2c_time_left")
-        if wdt_i2c_time_left > 99:
-            print("wdt_i2c_time_left is greater than 99 seconds at: " +
+        wdt_i2c_time_left = int(read_state(self, "gomspace.wdt_i2c_time_left"))
+        if wdt_i2c_time_left < 99:
+            print("wdt_i2c_time_left is less than 99 seconds at: " +
                   str(wdt_i2c_time_left))
 
-        counter_wdt_i2c = simulation.flight_controller.read_state(
-            "gomspace.counter_wdt_i2c")
+        counter_wdt_i2c = read_state(self, "gomspace.counter_wdt_i2c")
         print("counter_wdt_i2c is: " + str(counter_wdt_i2c))
-        counter_boot = simulation.flight_controller.read_state(
-            "gomspace.counter_boot")
+        counter_boot = read_state(self, "gomspace.counter_boot")
         print("counter_wdt_i2c is: " + str(counter_boot))
 
-        temp = [read_state(self, "gomspace.temp.output" + str(i))
+        temp = [int(read_state(self, "gomspace.temp.output" + str(i)))
                 for i in range(1, 5)]
         for n in range(0, len(temp)):
             temp_n = temp[n]
             if temp_n < 20 or out_n > 22:
-                print("Temp-" + n +
-                      " is out of room temperature range [20, 22] degC at: " + str(temp_n))
+                print("Temp-" + str(n) +
+                      " is out of room temperature range [20, 25] degC at: " + str(temp_n))
 
         bootcause = read_state(self, "gomspace.bootcause")
         print("bootcause is: " + str(bootcause))
@@ -89,28 +94,33 @@ class GomspaceCheckoutCase(SingleSatOnlyCase):
             if power_cycle_output_cmd[n] == power_cycle_output_cmd_updated[n]:
                 print("Could not update power_cycle_output" + str(n))
 
-        ppt_mode_cmd = read_state(self, "gomspace.pptmode_cmd")
+        ppt_mode_cmd = int(read_state(self, "gomspace.pptmode_cmd"))
+        ppt_mode_updated = int(write_state(
+            self, "gomspace.pptmode_cmd", (int(ppt_mode_cmd) + 1) % 2))
+        if ppt_mode_cmd == ppt_mode_updated:
+            print("Could not update pptmode")
 
-        heater_cmd = read_state(self, "gomspace.heater_cmd")
-        heater_cmd_updated = write_state(
-            self, "gomspace.heater_cmd", not heater_cmd)
+        heater_cmd = bool(read_state(self, "gomspace.heater_cmd"))
+        heater_cmd_updated = bool(write_state(
+            self, "gomspace.heater_cmd", not heater_cmd))
         if heater_cmd == heater_cmd_updated:
             print("Could not update heater")
 
-        counter_reset_cmd = read_state(self, "gomspace.counter_reset_cmd")
-        counter_reset_cmd_updated = write_state(
-            self, "gomspace_reset_cmd", not counter_reset_cmd)
+        counter_reset_cmd = bool(read_state(
+            self, "gomspace.counter_reset_cmd"))
+        counter_reset_cmd_updated = bool(write_state(
+            self, "gomspace.counter_reset_cmd", not counter_reset_cmd))
         if counter_reset_cmd == counter_reset_cmd_updated:
-            print("Could not update gomspace_reset")
+            print("Could not update counter_reset")
 
-        gs_reset_cmd = read_state(self, "gomspace.gs_reset_cmd")
-        gs_reset_cmd_updated = write_state(
-            self, "gomspace.gs_reset_cmd", not gs_reset_cmd)
+        gs_reset_cmd = bool(read_state(self, "gomspace.gs_reset_cmd"))
+        gs_reset_cmd_updated = bool(write_state(
+            self, "gomspace.gs_reset_cmd", not gs_reset_cmd))
         if gs_reset_cmd == gs_reset_cmd_updated:
             print("Could not update gs_reset")
 
-        gs_reboot_cmd = read_state(self, "gomspace.gs_reboot_cmd")
-        gs_reboot_cmd_updated = write_state(
-            self, "gomspace.gs_reboot_cmd", not gs_reset_cmd)
+        gs_reboot_cmd = bool(read_state(self, "gomspace.gs_reboot_cmd"))
+        gs_reboot_cmd_updated = bool(write_state(
+            self, "gomspace.gs_reboot_cmd", not gs_reboot_cmd))
         if gs_reboot_cmd == gs_reboot_cmd_updated:
             print("Could not update gs_reboot")
