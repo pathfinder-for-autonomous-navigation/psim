4c3a4f289872c563670713efe50f34a474d7f627
nhz2 <nhz2@cornell.edu>
2020-02-16 22:17:54 -0500

    added vectors to the animation

--- MATLAB/plot/plot_fancy_animation.m
+++ MATLAB/plot/plot_fancy_animation.m
@@ -1,6 +1,9 @@
 function plot_fancy_animation(main_state_trajectory)
 %PLOT_FANCY_ANIMATION creates a fancy animation from the simulation data.
-%uses the cesium viewer
-czml2html(main_state_trajectory2czml_struct(main_state_trajectory),"psim.html")
+%uses the cesium viewer saves "psim.html" file.
+vector_names=["sat2sun","antenna","docking face","magnetic field","position"];
+color_names=["r","b","g","y","k"];
+czml= main_state_trajectory2czml_struct(main_state_trajectory,vector_names,color_names);
+czml2html(czml,"psim.html",true);
 end
 
