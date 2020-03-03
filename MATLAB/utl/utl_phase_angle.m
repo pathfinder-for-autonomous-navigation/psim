48a0d516b46725a59e50c3ef451711ab11d38176
nhz2 <nhz2@cornell.edu>
2020-03-02 23:44:31 -0500

    Update utl_phase_angle.m

--- MATLAB/utl/utl_phase_angle.m
+++ MATLAB/utl/utl_phase_angle.m
@@ -4,8 +4,8 @@ function phase = utl_phase_angle(r1,v1,r2)
 % the vectors should be in any inertial frame with any units, 
 % the orbit plane must be well defined, ie, norm(cross(r1,v1))>0
 % phase is in radians range [-pi,pi]
-    DCM_hill_intertial = utl_inertial2hill(r1, v1);
-    r2_hill= DCM_hill_intertial*r2;
-    phase= atan2(r2_hill(2),r2_hill(1));
+DCM_hill_intertial = utl_inertial2hill(r1, v1);
+r2_hill= DCM_hill_intertial*r2;
+phase= atan2(r2_hill(2),r2_hill(1));
 end
 
