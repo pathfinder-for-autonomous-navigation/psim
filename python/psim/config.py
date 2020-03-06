13e1ef16e331454e8c4ea13decf45499b25875b5
kkrol27 <kyle.krol.27@gmail.com>
2020-03-06 10:38:11 -0500

    working updates
    Former-commit-id: 57c71edb1e86fd112744ce6da6ad6c887fd2b7ea [formerly f50cf85f35e8b638e7043efce8f2163fe1a0e5c6]
    Former-commit-id: 4b57a7d4e25ee806c87fa53f63f969a190ee6797
    Former-commit-id: 90ff1799855a8ec559d6fecfa197b64c737f6318

--- python/psim/config.py
+++ python/psim/config.py
@@ -2,37 +2,100 @@ import json
 import os
 import sys
 
+_base_directory = os.path.join(os.path.dirname(__file__), 'config')
 
-def _directories_generator(base_directory, directory):
-    """Streams the directories obtained from walking up the configuration tree
-    starting from dir and ending on base_dir (inclusive).
+
+def _get_full_path(path):
+    """Expands the user home directory and any symbollic links.
+
+    Args:
+        path:
+
+    Returns:
+        Fully expanded version of the path.
+    """
+    return os.path.abspath(os.path.expanduser(path))
+
+
+def _get_config_directories(directory, base_directory):
+    """Streams the directories obtained from walking up the configuration tree.
+
+    Args:
+        directory:
+        base_directory:
     """
     # Ensure the base directory exists
+    base_directory = _get_full_path(base_directory)
     if not os.path.isdir(base_directory):
-        raise RuntimeError('Invalid base configuration directory: ' + base_directory)
+        raise RuntimeError('Invalid base configuration directory: %s' % base_directory)
 
     # Ensure the specified directory is valid
-    directory = os.path.abspath(os.path.expanduser(directory))
+    directory = _get_full_path(directory)
     if not os.path.isdir(directory) or not directory.startswith(base_directory):
-        raise RuntimeError('Invalid configuration directory:' + directory)
+        raise RuntimeError('Invalid configuration directory: %s' % directory)
 
-    yield directory
-    while directory is not base_directory:
-        directory = os.path.dirname(directory)
+    while directory.startswith(base_directory):
         yield directory
+        directory = _get_full_path(os.path.dirname(directory))
 
 
-def load(directory):
+def _get_module_dictionaries(module, directory, base_directory):
+    """Streams the json dictionaries for the desired module encountered while
+    walking up the configuration tree.
+    
+    Args:
+        directory:
+        base_directory:
+        module:
     """
-    returns:
-        leader, follower
-    """
-    base_directory = os.path.join(os.path.dirname(os.path.abspath(os.path.expanduser(__file__))), 'config')
+    for _directory in _get_config_directories(base_directory, directory):
+        potential_json = os.path.join(_directory, module + ".json")
+        if os.path.isfile(potential_json):
+            with open(potential_json, 'r') as _json:
+                yield json.load(_json)
 
-    # Iterate through directories
-    print(base_directory)
-    for dir in _directories_generator(base_directory, directory):
-        print(directory)
+
+def load_module(module, directory, base_directory=_base_directory)
+    """
+    Args:
+        module:
+        directory:
+        base_directory:
+    
+    Returns:
+    """
+    def _search_for_sub_dictionary(dictionary, sub_dictionary):
+        _sub_dictionary = dict()
+        if (sub_dictionary in dictionary.keys()):
+            _sub_dictionary = dictionary['shared']
+            if not (type(_sub_dictionary) is dict):
+                raise RuntimeError('"%s" entry is expected to be a dictionary' % sub_dictionary)
+        
+        return _sub_dictionary
+
+    leader = dict()
+    follower = dict()
+    for dictionary in _get_module_dictionaries(module, directory, _base_directory):
+
+        # Look for shared, leader, and follower entries
+        _shared = _search_for_sub_dictionary(dictionary, 'shared')
+        _leader = _search_for_sub_dictionary(dictionary, 'leader')
+        _follower = _search_for_sub_dictionary(dictionary, 'follower')
+
+        # Prepare the new leader dictionary
+        leader.update(_shared)
+        leader.update(_leader)
+
+        # Prepare the new follower dictionary
+        follower.update(_shared)
+        follower.update(_follower)
+    
+    return leader, follower
+
+
+def load_modules(modules, directory, base_directory=_base_directory):
+    """
+    """
 
 
 load('psim/config/deployment')
