9dc484ace21517f17081e6287750a499a66a8914
nhz2 <nhz2@cornell.edu>
2020-03-09 15:10:54 -0400

    added install script and seperated out setup path

---
+++ MATLAB/install.m
@@ -0,0 +1,5 @@
+%Script to compile everything, and get data files, only call this on fresh
+%installs, or if you want to refresh everything.
+setup_path();
+%compile everything
+generate_mex_code();
