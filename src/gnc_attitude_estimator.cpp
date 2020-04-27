5ceba3eb6ddeb35c2df65ee6bbbafc08ca463c18
kkrol27 <kyle.krol.27@gmail.com>
2020-04-26 22:12:42 -0500

    new attitude estimator interface

--- src/gnc_attitude_estimator.cpp
+++ src/gnc_attitude_estimator.cpp
@@ -1,78 +1,80 @@
+//
+// MIT License
+//
+// Copyright (c) 2020 Pathfinder for Autonomous Navigation (PAN)
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
 /** @file gnc_attitude_estimator.cpp
  *  @author Kyle Krol */
 
 #include <gnc/attitude_estimator.hpp>
+#include <gnc/config.hpp>
 #include <gnc/constants.hpp>
-#include <gnc/environment.hpp>
-#include <gnc/utilities.hpp>
 
+#include <lin/core.hpp>
 #include <lin/generators/constants.hpp>
-
-#include <cmath>
-#include <limits>
 
 namespace gnc {
 
-AttitudeEstimatorState::AttitudeEstimatorState() { }
+AttitudeEstimatorState::AttitudeEstimatorState()
+: q_body_eci(lin::nans<lin::Vector4f>()),
+  gyro_bias(lin::nans<lin::Vector3f>()),
+  P(lin::nans<lin::Matrixf<6, 6>>()),
+  t(constant::nan),
+  is_valid(false) { }
 
 AttitudeEstimatorData::AttitudeEstimatorData()
-: t(std::numeric_limits<double>::quiet_NaN()),
-  r_ecef(lin::nans<decltype(r_ecef)>()),
-  b_body(lin::nans<decltype(b_body)>()),
-  s_body(lin::nans<decltype(s_body)>()),
-  w_body(lin::nans<decltype(w_body)>()) { }
+: r_ecef(lin::nans<lin::Vector3d>()),
+  b_body(lin::nans<lin::Vector3f>()),
+  s_body(lin::nans<lin::Vector3f>()),
+  w_body(lin::nans<lin::Vector3f>()),
+  t(constant::nan) { }
 
 AttitudeEstimate::AttitudeEstimate()
-: q_body_eci(lin::nans<decltype(q_body_eci)>()),
-  w_body(lin::nans<decltype(w_body)>()) { }
+: q_body_eci(lin::nans<lin::Vector4f>()),
+  gyro_bias(lin::nans<lin::Vector3f>()),
+  P(lin::nans<lin::Matrixf<6, 6>>()),
+  is_valid(false) { }
 
-static void estimate_q_body_eci(double t, lin::Vector3d const &r_ecef,
-    lin::Vector3f const &s_body, lin::Vector3f const &b_body,
-    lin::Vector4f &q_body_eci) {
-  lin::Vector4f q;
-  lin::Vector3f s_eci, b_eci;
-
-  // Default to NaNs
-  q_body_eci = lin::nans<lin::Vector4f>();
-
-  // Determine the magnetic field in ECI (it's given in ECEF)
-  env::magnetic_field(t, lin::Vector3f(r_ecef), b_eci);  // b_eci = b_ecef
-  env::earth_attitude(t, q);                             // q = q_ecef_eci
-  utl::quat_conj(q);                                     // q = q_eci_ecef
-  utl::rotate_frame(q, b_eci);                           // b_eci = b_eci
-
-  // Ensure the measured and expected magnetic field are large enough
-  float thresh = constant::b_noise_floor * constant::b_noise_floor;
-  if ((lin::fro(b_eci) < thresh) || (lin::fro(b_body) < thresh))
-    return;
-
-  // Determine the expected sun vector
-  env::sun_vector(t, s_eci);
-
-  // Ensure the sun vectors are unit vectors
+void attitude_estimator_reset(AttitudeEstimatorState &state,
+    double t, lin::Vector3d const &r_ecef, lin::Vector3f const &b_body,
+    lin::Vector3f const &s_body) {
   GNC_ASSERT_NORMALIZED(s_body);
-  GNC_ASSERT_NORMALIZED(s_eci);
 
-  // Perform triad
-  // Remember the value of q_body_eci will be set to NaNs if triad fails
-  utl::triad(s_eci, (b_eci / lin::norm(b_eci)).eval(), s_body,
-      (b_body / lin::norm(b_body)).eval(), q_body_eci);
+  // TODO : Implement this
+  state = AttitudeEstimatorState();
 }
 
-void estimate_attitude(AttitudeEstimatorState &state,
+void attitude_estimator_reset(AttitudeEstimatorState &state,
+    double t, lin::Vector4f const &q_eci_body) {
+  GNC_ASSERT_NORMALIZED(q_eci_body);
+
+  // TODO : Implement this
+  state = AttitudeEstimatorState();
+}
+
+void attitude_estimator_update(AttitudeEstimatorState &state,
     AttitudeEstimatorData const &data, AttitudeEstimate &estimate) {
-
-  // Default everything to NaN
+  // TODO : Implement this
+  state = AttitudeEstimatorState();
   estimate = AttitudeEstimate();
-
-  // Ensure the magnetic field vector, sun vector, ECEF position, and time were
-  // given
-  if (std::isnan(data.b_body(0)) || std::isnan(data.s_body(0)) ||
-      std::isnan(data.r_ecef(0)) || std::isnan(data.t))
-    return;
-
-  // Perform triad and pass the angular rate value through
-  estimate_q_body_eci(data.t, data.r_ecef, data.s_body, data.b_body, estimate.q_body_eci);
-  estimate.w_body = data.w_body;
 }
 }  // namespace gnc
