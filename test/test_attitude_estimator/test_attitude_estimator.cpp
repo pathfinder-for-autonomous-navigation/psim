7cc13f48fa5644750b253e7f9a3a2c084322780d
kkrol27 <kyle.krol.27@gmail.com>
2020-04-26 22:57:59 -0500

    initial attitude estimator tests

--- test/test_attitude_estimator/test_attitude_estimator.cpp
+++ test/test_attitude_estimator/test_attitude_estimator.cpp
@@ -39,7 +39,11 @@ static void test_attitude_estimate_constructor() {
 }
 
 static int test() {
+<<<<<<< HEAD
   UNITY_BEGIN();
+=======
+  UNITY_BEGIN()
+>>>>>>> 91e3de0... initial attitude estimator tests
   RUN_TEST(test_attitude_estimator_state_constructor);
   RUN_TEST(test_attitude_estimator_data_constructor);
   RUN_TEST(test_attitude_estimate_constructor);
