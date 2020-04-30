a715230754baf66dc5c8ca7746273d516588a05d
pan-master-chief <pan.ssds.qlocate@gmail.com>
2020-04-30 13:32:42 +0000

    messed around with energy PD

--- MATLAB/rendezvous/two_rendezvous_J2_dv_test.m
+++ MATLAB/rendezvous/two_rendezvous_J2_dv_test.m
@@ -1,151 +1,157 @@
 clearvars; clc;
 
-addpath('rendezvous');
+addpath('../');
 
 global const
 
 % initialize constants
 config();
-M = 4.0; % Mass (kg)
+M = 3.7; % Mass (kg)
 J_min = 2e-4; % Min impulse (Ns)
-J_max = 2e-2;%2e-2; % Max impulse (Ns)
-max_dv_thrust= J_max/M;
-V_rel   = 5; % Relative velocity at deployment (m/s)
-t_drift = 40.0 * 90.0 * 60.0; % Drift time (s)
-p = 0.7E-2;
-d = 1;
+J_max = 2e-2; % Max impulse (Ns)
+max_dv = J_max/M;
+v_rel   = 1; % Relative velocity at deployment (m/s)
+t_drift = 60.0 * 60.0; % Drift time (s)
+p = 0;
+d = 1e-8;
 thrust_noise_ratio = 0;
+dt_fire_min = 10 * 60; % [s] minimum time between firings
+% opt = odeset('RelTol', 1e-8, 'AbsTol', 1e-2, 'InitialStep', 0.1);
+
+% time
+tmax = 100 * 60 * 60; % [s]
+dt = 10; % [s]
+t = 0 : dt : tmax; % [s]
+N = length(t);
 
 a  = 6793137.0;  % Semimajor axis                        (m)
 e  = 0.001;      % Eccentricity                          (unitless)
-i  = 45*pi/180;  % Inclination angle                     (rad)
+I  = 45*pi/180;  % Inclination angle                     (rad)
 O  = 0.0;        % Right ascension of the ascending node (rad)
 o  = 0.0;        % Argument of perigee                   (rad)
 nu = 0*pi/180;   % True anamoly                          (rad)
 [   r1,...  % Position (m)   [eci]
     v1,...  % Velocity (m/s) [eci]
-] = utl_orb2rv(a * (1 - e*e), e, i, O, o, nu, const.mu);
+] = utl_orb2rv(a * (1 - e*e), e, I, O, o, nu, const.mu);
 
-n = utl_orbrate(a);      % Orbital rate
+n = utl_orbrate(a);      % [rad / s] Orbital rate
 w_hill = [0.0; 0.0; n];
 
 
 % Add initial velocity difference
 r2 = r1;
-v2 = randn(3, 1);
-v2 = v1 + V_rel * (v2 / norm(v2));
+% v2 = randn(3, 1);
+v2 = [1; 0.5; 0.25];
+v2 = v1 + v_rel * (v2 / norm(v2));
 
 % Allow spacecraft to drift apart
 [~, r1, v1, r2, v2] = drift_phase(r1, v1, r2, v2, t_drift);
 
-% hill frame transformation
-Q_transform = utl_eci2hill(r1, v1);
-r_hill = Q_transform * (r2 - r1);
-v_hill = Q_transform * (v2 - v1) - cross(w_hill, r_hill);
+% add measurement noise (zero for now)
+r1_measure = r1; % +0.1*randn(3,1);
+r2_measure = r2; % +0.1*randn(3,1);
+v1_measure = v1; % +0.001*randn(3,1);
+v2_measure = v2; % +0.001*randn(3,1);
 
 % initialize arrays
-X(:, 1) = [r_hill; v_hill];
-N = 1000; % orbits
-deltaenergies = zeros(N,1);
+X = zeros(6, N);
+deltaenergies = zeros(N, 1);
 u_vec = zeros(N, 1);
-dv_vec = zeros(1, 3);
+r_eci = zeros(6, N);
+dv_vec = zeros(3, N);
+r_fire = [];
+dv_des = zeros(N, 1);
+de_point_mass = zeros(N, 1);
+energy1_vec = zeros(N, 1);
+energy1_j1_vec = zeros(N, 1);
+energy2_vec = zeros(N, 1);
+energy2_j1_vec = zeros(N, 1);
+
+% Calculate initial state
+Q_eci_hill = utl_eci2hill(r1, v1);
+r_hill = Q_eci_hill * (r2 - r1);
+v_hill = Q_eci_hill * (v2 - v1) - cross(w_hill, r_hill);
+X(:, 1) = [r_hill; v_hill];
+[~,potential,~]= env_gravity(0,r1);
+energy1= 0.5*dot(v1,v1)-potential;
+[~,potential,~]= env_gravity(0,r2);
+energy2= 0.5*dot(v2,v2)-potential;
+deltaenergies(1)=energy2-energy1;
+r_eci(:, 1) = [r1; r2];
+% energy1_point_mass = norm(v1)^2 / 2 - const.mu / norm(r1);
+% energy2_point_mass = norm(v2)^2 / 2 - const.mu / norm(r2);
+% de_point_mass(1) = energy2_point_mass - energy1_point_mass;
+energy1_vec(1) = energy1;
+% energy1_j1_vec(1) = energy1_point_mass;
+energy2_vec(1) = energy2;
+% energy2_j1_vec(1) = energy2_point_mass;
+
+t_fire = -dt_fire_min; % set so we fire on the first orbit
 
 for i = 1:(N - 1)
     
-    if ~mod(i, 10)
+    if ~mod(i, 1000)
         fprintf('orbit %d / %d\n', i, N);
     end
     
-    % add measurement noise (zero for now)
-    r1_measure=r1; % +0.1*randn(3,1);
-    r2_measure=r2; % +0.1*randn(3,1);
-    v1_measure=v1; % +0.001*randn(3,1);
-    v2_measure=v2; % +0.001*randn(3,1);
+    % calculate follower orbital elements
+    a = -const.mu / (2 * energy2);
+    n = utl_orbrate(a);
+    M = n * t(i);
+    M = mod(M, 2 * pi);
     
-    % Calculate our hill state
-    Q_eci_hill = utl_eci2hill(r1_measure, v1_measure);
-    r_hill = Q_eci_hill * (r2_measure - r1_measure);
-    v_hill = Q_eci_hill * (v2_measure - v1_measure) - cross(w_hill, r_hill);
+    % calculate eccentric anomaly
+    del = 1;
+    E = M / (1 - e);
     
-    % point B
-    [~,potential,~] = env_gravity(0,r1_measure);
-    energy1 = 0.5*dot(v1_measure,v1_measure)-potential;
-    [~,potential,~] = env_gravity(0,r2_measure);
-    energy2 = 0.5*dot(v2_measure,v2_measure)-potential;
-%     if (norm(r_hill(2))<10E3 && abs(energy1-energy2)<200)
-%         %do cw
-% %         fprintf('cw\n')
-%         x_dv=-v_hill(1);
-%         y_x= r_hill(2);
-%         y_v= (-12*pi*r_hill(1)-1/n*6*pi*v_hill(2));
-%         y_maxu= 1/n*6*pi*sqrt(2)/2*max_dv_thrust;
-%         y_u= discrete_suicide_burn(y_maxu, y_v,y_x);
-%         y_dv= y_u/(-1/n*6*pi);
-%         dv=y_dv*v2_measure/norm(v2_measure);
-%     else
-    %just use energy pd
-    x_dv=0;
-    %zero energy diff pd control
-    pterm= p*r_hill(2);
-    dterm= d*(energy1-energy2);
-    dv=(pterm+dterm)*v2_measure;
-    dv= max(min(dv,sqrt(2)/2*max_dv_thrust),-sqrt(2)/2*max_dv_thrust);
-%     end
-    u_now_hill= [x_dv;0;0;];
+    if E > sqrt(6 * (1 - e) / e)
+        E = (6 * M / e) ^ (1/3);
+    end
     
-    % Apply actuation
-    dv= Q_eci_hill' * u_now_hill + dv + normal_plane_correction(r1_measure,v1_measure,r2_measure,v2_measure,max_dv_thrust);
-    dv= dv/norm(dv)*min(norm(dv),max_dv_thrust);
-    dv= dv+thrust_noise_ratio*norm(dv)*randn(3,1);
-    v2 = v2 + dv;
-    dv_vec = [dv_vec; dv'];
+    while del > eps(2 * pi)
+        
+        E = E - (M - E + e * sin(E)) / (e * cos(E) - 1);
+        del = max(abs(M - (E - e * sin(E))));
+        
+    end
     
-    % Simulate dynamics
-    dt_fire= 3*pi/2/n;
-    [~, r1, v1, r2, v2] = drift_phase(r1, v1, r2, v2, dt_fire);
-    r1_measure=r1+0.1*randn(3,1);
-    r2_measure=r2+0.1*randn(3,1);
-    v1_measure=v1+0.001*randn(3,1);
-    v2_measure=v2+0.001*randn(3,1);
-    
-    % Calculate our hill state
-    Q_eci_hill = utl_eci2hill(r1_measure, v1_measure);
-    r_hill = Q_eci_hill * (r2_measure - r1_measure);
-    v_hill = Q_eci_hill * (v2_measure - v1_measure) - cross(w_hill, r_hill);
-    
-    % point A
-    [~,potential,~]= env_gravity(0,r1_measure);
-    energy1= 0.5*dot(v1_measure,v1_measure)-potential;
-    [~,potential,~]= env_gravity(0,r2_measure);
-    energy2= 0.5*dot(v2_measure,v2_measure)-potential;
-%     if (norm(r_hill(2))<10E3 && abs(energy1-energy2)<200)
-%         %do cw
-% %         fprintf('cw\n')
-%         x_dv= -n*(4*r_hill(1)+1/n*v_hill(1)+2/n*v_hill(2));
-%         x_dv=max(min(x_dv,sqrt(2)/2*max_dv_thrust),-sqrt(2)/2*max_dv_thrust);
-%         dv=0;
-%     else
-    %just use energy
-    x_dv=0;
-    %zero energy diff
-    %zero energy diff pd control
-    pterm= p*r_hill(2);
-    dterm= d*(energy1-energy2);
-    dv=(pterm+dterm)*v2_measure;
-    dv= max(min(dv,sqrt(2)/2*max_dv_thrust),-sqrt(2)/2*max_dv_thrust);
-% end
-    u_now_hill= [x_dv;0;0];
-    
-    % Apply actuation
-    dv= Q_eci_hill' * u_now_hill+dv+normal_plane_correction(r1_measure,v1_measure,r2_measure,v2_measure,max_dv_thrust);
-    dv= dv/norm(dv)*min(norm(dv),max_dv_thrust);
-    dv= dv+thrust_noise_ratio*norm(dv)*randn(3,1);
-    v2 = v2 + dv;
-    dv_vec = [dv_vec; dv'];
-    
-    % Simulate dynamics
-    dt_fire= pi/2/n;
-    [~, r1, v1, r2, v2] = drift_phase(r1, v1, r2, v2, dt_fire);
+    % check if follower is at a firing point
+    if (abs(E - pi / 4) < 0.01 || abs(E - 3 * pi / 4) < 0.01) && t(i) - t_fire > dt_fire_min
+        
+        % record firing time and position
+        t_fire = t(i);
+        r_fire = [r_fire, [r1; r2]];
+        
+        % energy PD controller
+        pterm = p * r_hill(2);
+        dterm = -d * (energy2 - energy1);
+        dv = (pterm + dterm) * v2;
+        dv_des(i) = norm(dv);
+%         dv = max(min(dv, sqrt(2) / 2 * max_dv), -sqrt(2) / 2 * max_dv);
+        u_now_hill = [0; 0; 0];
+        
+%         % normal plane correction
+%         dv_plane = normal_plane_correction(r1, v1, r2, v2, max_dv_thrust);
+        
+        % Apply actuation
+        dv = Q_eci_hill' * u_now_hill + dv;
+%         dv = dv / norm(dv) * min(norm(dv), max_dv);
+        
+        if norm(dv) > max_dv
+            dv = max_dv * dv / norm(dv);
+        end
+        
+        v2 = v2 + dv;
+        dv_vec(:, i) = dv;
+        
+    end
+
+    % simulate dynamics
+    y = utl_ode4(@frhs, [0.0, dt], [r1; v1; r2; v2]);
+    r1 = y(end, 1:3)';
+    v1 = y(end, 4:6)';
+    r2 = y(end, 7:9)';
+    v2 = y(end, 10:12)';
     
     % Calculate new state
     Q_eci_hill = utl_eci2hill(r1, v1);
@@ -156,48 +162,67 @@ for i = 1:(N - 1)
     energy1= 0.5*dot(v1,v1)-potential;
     [~,potential,~]= env_gravity(0,r2);
     energy2= 0.5*dot(v2,v2)-potential;
-    deltaenergies(i)=energy2-energy1;
+    deltaenergies(i + 1)=energy2-energy1;
+    r_eci(:, i + 1) = [r1; r2];
+    
+%     energy1_point_mass = norm(v1)^2 / 2 - const.mu / norm(r1);
+%     energy2_point_mass = norm(v2)^2 / 2 - const.mu / norm(r2);
+%     de_point_mass(i + 1) = energy2_point_mass - energy1_point_mass;
+    energy1_vec(i + 1) = energy1;
+    energy2_vec(i + 1) = energy2;
+%     energy1_j1_vec(i + 1) = energy1_point_mass;
+%     energy2_j1_vec(i + 1) = energy2_point_mass;
     
 end
 
 % plots
 
+% follower ECI position
+figure;
+plot(r_eci(1, :), r_eci(2, :)); hold on
+plot(r_fire(1, :), r_fire(2, :), 'o')
+xlabel('x ECI [m]')
+ylabel('y ECI [m]')
+title('follower ECI position')
+
+% relative position norm
 figure
 hold on
-plot(X(1, :), '-r')
-plot(X(2, :), '-g')
-plot(X(3, :), '-b')
+plot(t, X(1, :), '-r')
+plot(t, X(2, :), '-g')
+plot(t, X(3, :), '-b')
 hold off
 title('Relative Position of Satellite Two (rgb ~ xyz LVLH)')
 xlabel('Time (s)')
 ylabel('Position (m)')
 
-% relative position
+% relative position norm
 rel_pos = zeros(1, N);
 
 for i = 1 : N
     
-    rel_pos(i) = norm(X(1:3, i));
+    rel_pos(i) = norm(X(1 : 3, i));
     
 end
 
 figure;
-plot(rel_pos)
-xlabel('orbit')
+plot(t, rel_pos)
+xlabel('t [s]')
 ylabel('position [m]')
 title('relative position norm')
 
+% relative velocity
 figure
 hold on
-plot(X(4, :), '-r')
-plot(X(5, :), '-g')
-plot(X(6, :), '-b')
+plot(t, X(4, :), '-r')
+plot(t, X(5, :), '-g')
+plot(t, X(6, :), '-b')
 hold off
 title('Relative Velocity of Satellite Two (rgb ~ xyz LVLH)')
 xlabel('Time (s)')
 ylabel('Velocity (m/s)')
 
-% relative velocity
+% relative velocity norm
 rel_vel = zeros(1, N);
 
 for i = 1 : N
@@ -207,21 +232,33 @@ for i = 1 : N
 end
 
 figure;
-plot(rel_vel)
-xlabel('orbit')
+plot(t, rel_vel)
+xlabel('t [s]')
 ylabel('position [m/s]')
 title('relative velocity norm')
 
+% energy difference
 figure;
-plot(deltaenergies);
+plot(t, deltaenergies)
 xlabel('t [s]')
 title('delta energies')
 
+% figure;
+% plot(t, energy1_vec, 'b--'); hold on
+% plot(t, energy1_j1_vec, 'b')
+% plot(t, energy2_vec, 'r--')
+% plot(t, energy2_j1_vec, 'r')
+% xlabel('t [s]')
+% ylabel('energy [J]')
+% title('follower and leader energy')
+% legend('follower J4', 'follower J1', 'follower J4', 'follower J1')
+
+% dv
 figure;
-plot(dv_vec(:, 1)); hold on
-plot(dv_vec(:, 2))
-plot(dv_vec(:, 3))
-xlabel('burn')
+plot(t, dv_vec(1, :)); hold on
+plot(t, dv_vec(2, :))
+plot(t, dv_vec(3, :))
+xlabel('t [s]')
 ylabel('\Delta v [m/s]')
 legend('x', 'y', 'z')
 title('\Delta v')
@@ -230,23 +267,44 @@ dv_norm = zeros(N, 1);
 
 for i = 1 : N
     
-    dv_norm(i) = norm(dv_vec(i, :));
+    dv_norm(i) = norm(dv_vec(:, i));
     
 end
 
 dv_total = cumsum(dv_norm);
 
 figure;
-plot(dv_norm)
-xlabel('burn')
+plot(t, dv_norm)
+xlabel('t [s]')
 ylabel('\Delta v [m/s]')
 title('\Delta v norm')
 
 figure;
-plot(dv_total)
-xlabel('burn')
+plot(t, dv_total)
+xlabel('t [s]')
 ylabel('\Delta v [m/s]')
 title('Cumulative \Delta v')
+
+figure;
+plot(t, dv_des); hold on
+plot(t, max_dv * ones(N, 1))
+xlabel('t [s]')
+ylabel('\Delta v [m/s]')
+title('Desired \Delta v norm')
+
+function dy = frhs(~, y)
+
+dy = zeros(12, 1);
+
+[gx, gy, gz] = gravityzonal(y(1:3)', 'Earth', 4, 'Error');
+dy(1:3, 1) = y(4:6, 1);
+dy(4:6, 1) = [gx; gy; gz];
+
+[gx, gy, gz] = gravityzonal(y(7:9)', 'Earth', 4, 'Error');
+dy(7:9, 1) = y(10:12, 1);
+dy(10:12, 1) = [gx; gy; gz];
+
+end
 
 function normal_plane_dv2= normal_plane_correction(r1,v1,r2,v2,max_dv_thrust)
     %calc average angular momentum
