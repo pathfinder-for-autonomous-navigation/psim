d503dcdcd846430b71272d11e469004bb02c9244
kkrol27 <33558436+kkrol27@users.noreply.github.com>
2020-05-01 00:28:12 -0500

    Update include/gnc/utilities.hpp
    Co-authored-by: nhz2 <39104088+nhz2@users.noreply.github.com>

--- include/gnc/utilities.hpp
+++ include/gnc/utilities.hpp
@@ -52,7 +52,7 @@ constexpr void quat_cross_mult(lin::Vector<T, 4> const &q1,
  *  @param[in]  a 
  *  @param[in]  f
  *  @param[out] p Generalized Rodrigues parameter representation.
- *  Converts a quaternion attitude representation to generalized rodriquez
+ *  Converts a quaternion attitude representation to generalized Rodrigues
  *  parameters. There is no explicit handling of NaNs built into this function;
  *  however, a finite input will always yield a finite result.
  *  Source: "Unscented Filtering for Spacecraft Attitude Estimation"
