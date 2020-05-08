af8a25421a6432e833b61ae43fc1339649180269
Shihao Cao <shehowcow@gmail.com>
2020-05-08 17:51:22 -0400

    fix is valid

--- MATLAB/attitude_filter/helper_functions/estimator_update.cpp
+++ MATLAB/attitude_filter/helper_functions/estimator_update.cpp
@@ -28,6 +28,12 @@ public:
         typed_array_to_lin_vec(state.x, inputs[1]);
         typed_array_to_lin_mat(state.P, inputs[2]);
         state.t = inputs[3][0];
+        
+        // assume always valid
+        state.is_valid = (lin::all(lin::isfinite(state.q)) &&
+            lin::all(lin::isfinite(state.x)) &&
+            lin::all(lin::isfinite(state.P)) &&
+            lin::isfinite(state.t)) 
 
         // empty estimate
         gnc::AttitudeEstimate estimate = gnc::AttitudeEstimate();
