cc4f9038416e38660e744c51cdefe0a20bdda334
nhz2 <nhz2@cornell.edu>
2020-05-11 18:37:57 -0400

    testing lin

--- estimatortest/pythonwrapper/lin_ext.cpp
+++ estimatortest/pythonwrapper/lin_ext.cpp
@@ -10,6 +10,8 @@
 #include <orb/Orbit.h>
 #include <lin/core.hpp>
 #include <lin/generators.hpp>
+#include <sstream>
+
 
 namespace py = pybind11;
 
@@ -54,7 +56,9 @@ namespace py = pybind11;
             for (lin::size_t i=0; i<n; i++){ \
                 out += "\n"; \
                 for (lin::size_t j=0; j<m; j++){ \
-                    out += std::to_string(x(i,j))+","; \
+                    std::ostringstream ss; \
+                    ss << x(i,j); \
+                    out += ss.str()+","; \
                 } \
             } \
             return out; \
@@ -113,7 +117,9 @@ namespace py = pybind11;
             for (lin::size_t i=0; i<n; i++){ \
                 out += "\n"; \
                 for (lin::size_t j=0; j<m; j++){ \
-                    out += std::to_string(x(i,j))+","; \
+                    std::ostringstream ss; \
+                    ss << x(i,j); \
+                    out += ss.str()+","; \
                 } \
             } \
             return out; \
@@ -141,6 +147,52 @@ namespace py = pybind11;
  * Contains python wrappers of used lin types. 
  */ 
 void init_lin_ext(py::module &m){
-    WRAPLINMATRIX("lin_Matrix6x6d",m,double,6,6,6,6);
+    WRAPLINVECTOR("lin_Vector2f",m,float,2,2);
+    WRAPLINVECTOR("lin_Vector3f",m,float,3,3);
+    WRAPLINVECTOR("lin_Vector4f",m,float,4,4);
+    WRAPLINVECTOR("lin_Vector5f",m,float,5,5);
+    WRAPLINVECTOR("lin_Vector6f",m,float,6,6);
+    WRAPLINVECTOR("lin_Vector7f",m,float,7,7);
+    WRAPLINVECTOR("lin_Vector8f",m,float,8,8);
+    WRAPLINVECTOR("lin_Vector9f",m,float,9,9);
+    WRAPLINVECTOR("lin_Vector10f",m,float,10,10);
+    WRAPLINVECTOR("lin_Vector11f",m,float,11,11);
+    WRAPLINVECTOR("lin_Vector12f",m,float,12,12);
+
+    WRAPLINVECTOR("lin_Vector2d",m,double,2,2);
     WRAPLINVECTOR("lin_Vector3d",m,double,3,3);
+    WRAPLINVECTOR("lin_Vector4d",m,double,4,4);
+    WRAPLINVECTOR("lin_Vector5d",m,double,5,5);
+    WRAPLINVECTOR("lin_Vector6d",m,double,6,6);
+    WRAPLINVECTOR("lin_Vector7d",m,double,7,7);
+    WRAPLINVECTOR("lin_Vector8d",m,double,8,8);
+    WRAPLINVECTOR("lin_Vector9d",m,double,9,9);
+    WRAPLINVECTOR("lin_Vector10d",m,double,10,10);
+    WRAPLINVECTOR("lin_Vector11d",m,double,11,11);
+    WRAPLINVECTOR("lin_Vector12d",m,double,12,12);
+
+    WRAPLINMATRIX("lin_Matrix2x2f",m,float,2,2,2,2);
+    WRAPLINMATRIX("lin_Matrix3x3f",m,float,3,3,3,3);
+    WRAPLINMATRIX("lin_Matrix4x4f",m,float,4,4,4,4);
+    WRAPLINMATRIX("lin_Matrix5x5f",m,float,5,5,5,5);
+    WRAPLINMATRIX("lin_Matrix6x6f",m,float,6,6,6,6);
+    WRAPLINMATRIX("lin_Matrix7x7f",m,float,7,7,7,7);
+    WRAPLINMATRIX("lin_Matrix8x8f",m,float,8,8,8,8);
+    WRAPLINMATRIX("lin_Matrix9x9f",m,float,9,9,9,9);
+    WRAPLINMATRIX("lin_Matrix10x10f",m,float,10,10,10,10);
+    WRAPLINMATRIX("lin_Matrix11x11f",m,float,11,11,11,11);
+    WRAPLINMATRIX("lin_Matrix12x12f",m,float,12,12,12,12);
+
+    WRAPLINMATRIX("lin_Matrix2x2d",m,double,2,2,2,2);
+    WRAPLINMATRIX("lin_Matrix3x3d",m,double,3,3,3,3);
+    WRAPLINMATRIX("lin_Matrix4x4d",m,double,4,4,4,4);
+    WRAPLINMATRIX("lin_Matrix5x5d",m,double,5,5,5,5);
+    WRAPLINMATRIX("lin_Matrix6x6d",m,double,6,6,6,6);
+    WRAPLINMATRIX("lin_Matrix7x7d",m,double,7,7,7,7);
+    WRAPLINMATRIX("lin_Matrix8x8d",m,double,8,8,8,8);
+    WRAPLINMATRIX("lin_Matrix9x9d",m,double,9,9,9,9);
+    WRAPLINMATRIX("lin_Matrix10x10d",m,double,10,10,10,10);
+    WRAPLINMATRIX("lin_Matrix11x11d",m,double,11,11,11,11);
+    WRAPLINMATRIX("lin_Matrix12x12d",m,double,12,12,12,12);
+
 }
