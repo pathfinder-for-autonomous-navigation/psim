1d01e7d337a98c0496c0aca0dbf2e8d98d923c40
fatimayousuf <fatima.yousuf.0@gmail.com>
2020-09-22 18:58:09 -0700

    working on mex wrapper for orbit controller

--- MATLAB/orbit_controller/control_orbit.cpp
+++ MATLAB/orbit_controller/control_orbit.cpp
@@ -15,32 +15,75 @@ public:
     void operator()(matlab::mex::ArgumentList outputs, matlab::mex::ArgumentList inputs) {
         checkArguments(outputs, inputs);
 
-        static constexpr double mass = 3.7;
-        static constexpr double k_p = 1.0e-6;
-        static constexpr double k_d = 5.0e-2;
-        static constexpr double k_e = 5.0e-5;
-        static constexpr double k_h = 2.0e-3;
+        // assemble lin data
+        gnc::OrbitControllerData data_in = OrbitControllerData::OrbitControllerData();
+        
+        data_in.t = inputs[11][0];
+        typed_array_to_lin_vec(data_in.r_ecef, inputs[12]);
+        typed_array_to_lin_vec(data_in.v_ecef, inputs[13]);
+        typed_array_to_lin_vec(data_in.dr_ecef, inputs[14]);
+        typed_array_to_lin_vec(data_in.dv_ecef, inputs[15]);
 
-        gnc::OrbitControllerState state = gnc::OrbitControllerState();
-        gnc::OrbitControllerData data = gnc::OrbitControllerData();
-        gnc::OrbitActuation actuation = gnc::OrbitActuation();
-        gnc::mex_control_orbit(state, data, actuation, mass, k_p, k_d, k_e, k_h);
+        // assemble lin state - serves as a calculation buffer
+        gnc::OrbitControllerState state = OrbitControllerState::OrbitControllerState();
+
+        state.t_last_firing = inputs[0][0];
+        typed_array_to_lin_vec(state.this_r_ecef0, inputs[1]);
+        typed_array_to_lin_vec(state.that_r_ecef0, inputs[2]);
+        typed_array_to_lin_vec(state.this_r_hat, inputs[3]);
+        typed_array_to_lin_vec(state.this_v_ecef0, inputs[4]);
+        typed_array_to_lin_vec(state.that_v_ecef0, inputs[5]);
+        typed_array_to_lin_vec(state.this_v_hat, inputs[6]);
+        typed_array_to_lin_vec(state.this_h_ecef0, inputs[7]);
+        typed_array_to_lin_vec(state.that_h_ecef0, inputs[8]);
+        typed_array_to_lin_vec(state.this_h_hat, inputs[9]);
+        typed_array_to_lin_mat(state.DCM_hill_ecef0, inputs[10]);
+
+        // empty actuation
+        gnc::OrbitActuation actuation = OrbitActuation::OrbitActuation();
+
+        gnc::control_orbit(state, data_in, actuation, mass, p, d, energy_gain, h_gain);
         
         ArrayFactory f;
 
         // will only contain one element
         StructArray S = f.createStructArray({1,1}, 
-            {"q", "x","P","t"});
+            {"t_last_firing",
+             "r2", "r1", "r2hat", 
+             "v2", "v1", "v2hat",
+             "h2", "h1", "h2hat",
+             "dcm_hill_ecef0"});
 
-        S[0]["q"] = create_from_lin_vec(f, state.q);
-        S[0]["x"] = create_from_lin_vec(f, state.x);
-        S[0]["P"] = create_from_lin_mat(f, state.P);
+        S[0]["t_last_firing"] = create_from_lin_vec(f, state.t_last_firing);
+        S[0]["r2"] = create_from_lin_vec(f, state.this_r_ecef0);
+        S[0]["r1"] = create_from_lin_vec(f, state.that_r_ecef0);
+        S[0]["r2hat"] = create_from_lin_vec(f, state.this_r_hat);
+        S[0]["v2"] = create_from_lin_vec(f, state.this_v_ecef0);
+        S[0]["v1"] = create_from_lin_vec(f, state.that_v_ecef0);
+        S[0]["v2hat"] = create_from_lin_vec(f, state.this_v_hat);
+        S[0]["h2"] = create_from_lin_vec(f, state.this_h_ecef0);
+        S[0]["h1"] = create_from_lin_vec(f, state.that_h_ecef0);
+        S[0]["h2hat"] = create_from_lin_vec(f, state.this_h_hat);
+        S[0]["dcm_hill_ecef0"] = create_from_lin_vec(f, state.DCM_hill_ecef0);
 
-        lin::Matrixd<1,1> dummy = {state.t};
-        S[0]["t"] = create_from_lin_mat(f, dummy);
-
-        //outputs[0] is the struct
+        //outputs[0] is the state struct
         outputs[0] = S;
+
+        outputs[1] = create_from_lin_vec(f, actuation.J_ecef);
+        outputs[2] = create_from_lin_vec(f, actuation.phase_till_next_node);
     }
+
+    void checkArguments(matlab::mex::ArgumentList outputs, matlab::mex::ArgumentList inputs) {
+        std::shared_ptr<matlab::engine::MATLABEngine> matlabPtr = getEngine();
+        matlab::data::ArrayFactory factory;
+
+        // assuming t, then q
+
+        if (inputs.size() != 16) {
+            matlabPtr->feval(u"error", 
+                0, std::vector<matlab::data::Array>({ factory.createScalar("16 inputs required") }));
+        }
+    }
+
 
 }
---
+++ MATLAB/orbit_controller/make_mex_orbit_controller.m
@@ -0,0 +1,32 @@
+function [orbit_controller] = make_mex_orbit_controller()
+
+    orbit_controller.control = @control;
+
+end
+
+function [out_state, J_ecef, phase_till_next_node] = control(state, t, r_ecef, v_ecef, dr_ecef, dv_ecef)
+
+    assert(isequal(size(t),[1,1]), "t must be a scalar")
+    assert(isequal(size(r_ecef),[3,1]), "r_ecef must be a 3x1 vector")
+    assert(isequal(size(v_ecef),[3,1]), "v_ecef must be a 3x1 vector")
+    assert(isequal(size(dr_ecef),[3,1]), "dr_ecef must be a 3x1 vector")
+    assert(isequal(size(dv_ecef),[3,1]), "dv_ecef must be a 3x1 vector")
+
+    assert(isequal(size(state.t_last_firing),[1,1]), "state.t_last_firing must be a scalar")
+    assert(isequal(size(state.this_r_ecef0),[3,1]), "state.this_r_ecef0 must be a 3x1 vector")
+    assert(isequal(size(state.that_r_ecef0),[3,1]), "state.that_r_ecef0 must be a 3x1 vector")
+    assert(isequal(size(state.this_r_hat),[3,1]), "state.this_r_hat must be a 3x1 vector")
+    assert(isequal(size(state.this_v_ecef0),[3,1]), "state.this_v_ecef0 must be a 3x1 vector")
+    assert(isequal(size(state.that_v_ecef0),[3,1]), "state.that_v_ecef0 must be a 3x1 vector")
+    assert(isequal(size(state.this_v_hat),[3,1]), "state.this_v_hat must be a 3x1 vector")
+    assert(isequal(size(state.this_h_ecef0),[3,1]), "state.this_h_ecef0 must be a 3x1 vector")
+    assert(isequal(size(state.that_h_ecef0),[3,1]), "state.that_h_ecef0 must be a 3x1 vector")
+    assert(isequal(size(state.this_h_hat),[3,1]), "state.this_h_hat must be a 3x1 vector")
+    assert(isequal(size(state.DCM_hill_ecef0),[3,3]), "state.DCM_hill_ecef0 must be a 3x3 vector")
+
+    [out_state, J_ecef, phase_till_next_node] = control_orbit(state.t_last_firing, ... 
+                                                                state.this_r_ecef0, state.that_r_ecef0, state.this_r_hat, ...
+                                                                state.this_v_ecef0, state.that_v_ecef0, state.this_v_hat, ...
+                                                                state.this_h_ecef0, state.that_h_ecef0, state.this_h_hat, ...
+                                                                state.DCM_hill_ecef0, t, r_ecef, b_body, s_body, w_body);
+end
