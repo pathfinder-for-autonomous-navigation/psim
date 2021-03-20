0c0bdb8a94d7af1cf669322658935f98317e0200
Govind Chari <govindchari1@gmail.com>
2021-03-20 14:51:57 -0400

    Created orbit controller unit test

---
+++ python/test/test_orbit_controller.py
@@ -0,0 +1,24 @@
+from psim import Configuration, sims, Simulation
+
+import lin
+import pytest
+
+
+def test_orbit_controller():
+    """Test the orbit controller.
+    This boots the simulation starting in standby (after detumbling)
+    """
+    configs = ['sensors/base', 'truth/base', 'truth/standby']
+    configs = ['config/parameters/' + f + '.txt' for f in configs]
+
+    config = Configuration(configs)
+    sim = Simulation(sims.OrbitControllerTest, config)
+
+    #The spacecrafts are given 10 days to rendezvous
+    timeout = 10 * 24 * 3600 * 1e9
+    threshold = 0.5
+    sim.step()
+
+    while sim['truth.leader.hill.dr.norm'] > threshold:
+        assert sim['truth.t.ns'] < timeout , 'Spacecrafts failed to rendezvous in alloted time'
+        sim.step()
