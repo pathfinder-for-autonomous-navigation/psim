904d65a75af8d5d615db9f4ad7794004a4e4a27e
Govind Chari <govindchari1@gmail.com>
2021-04-13 20:27:23 -0400

    Resolved Merge Conflicts

--- src/psim/fc/orbit_controller.cpp
+++ src/psim/fc/orbit_controller.cpp
@@ -56,13 +56,9 @@ void OrbitController::step() {
   auto const &v_ecef = fc_satellite_orbit_v->get();
   auto const &other_r_ecef = truth_other_orbit_r_ecef->get();
   auto const &other_v_ecef = truth_other_orbit_v_ecef->get();
-<<<<<<< HEAD
   auto const &fire_time_far = fc_satellite_fire_time_far.get();
   auto const &fire_time_near = fc_satellite_fire_time_near.get();
   auto const &cdgps_dr = sensors_satellite_cdgps_dr->get();
-=======
-  auto const &fire_time = fc_satellite_fire_time.get();
->>>>>>> master
 
   Vector3 dr_ecef;
   Vector3 dv_ecef;
@@ -98,7 +94,6 @@ void OrbitController::step() {
     data.v_ecef = v_ecef;
     data.dr_ecef = prev_dr_ecef;
     data.dv_ecef = prev_dv_ecef;
-<<<<<<< HEAD
 
     if (lin::all(lin::isfinite(cdgps_dr))) {
       data.p = 1.0e-6 / 6; // Make "6" a parameter
@@ -111,8 +106,6 @@ void OrbitController::step() {
       data.energy_gain = 5.0e-5; // Energy gain                   (J)
       data.h_gain = 2.0e-3;      // Angular momentum gain         (kg m^2/sec)
     }
-=======
->>>>>>> master
 
     gnc::OrbitActuation actuation;
     gnc::control_orbit(_orbit_controller, data, actuation);
